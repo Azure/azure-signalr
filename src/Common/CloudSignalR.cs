31e77cae4206c03383dfd6bdb64b888e14b15f56
Hongjiang Zhang <honzhan@microsoft.com>
2018-04-17 16:26:34 +0800

    Service protocol for making refactoring version work
    The service protocol still needs to be refactoring in next step.

---
+++ src/Common/CloudSignalR.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.SignalR
+{
+    public class CloudSignalR
+    {
+        public static SignalRServiceContext<THub> CreateServiceContext<THub>() where THub : Hub
+        {
+            var hubMessageSender = ServiceProvider.GetRequiredService<IHubMessageSender>();
+            var connectionServiceProvider = ServiceProvider.GetRequiredService<IConnectionServiceProvider>();
+
+            var signalrServiceHubContext = new SignalRServiceHubContext<THub>(connectionServiceProvider, hubMessageSender);
+            return new SignalRServiceContext<THub>(connectionServiceProvider, signalrServiceHubContext);
+        }
+
+        internal static void ConfigureAuthorization(Action<AuthorizationOptions> configure)
+        {
+            if (configure != null) _authorizationConfigure = configure;
+        }
+
+        private static Action<AuthorizationOptions> _authorizationConfigure = _ => { };
+
+        private static IServiceProvider _externalServiceProvider = null;
+
+        private static IServiceCollection _externalServiceCollection = null;
+
+        private static readonly Lazy<IServiceProvider> _internalServiceProvider =
+            new Lazy<IServiceProvider>(
+                () =>
+                {
+                    var serviceCollection = new ServiceCollection();
+                    var signalRServerBuilder = serviceCollection.AddSignalR().AddAzureSignalR();
+                    signalRServerBuilder.Services.AddLogging();
+                    signalRServerBuilder.Services.AddAuthorization(_authorizationConfigure);
+                    return signalRServerBuilder.Services.BuildServiceProvider();
+                });
+
+        internal static IServiceProvider ServiceProvider
+        {
+            get => _externalServiceProvider ?? (_externalServiceCollection != null ? _externalServiceCollection.BuildServiceProvider() : _internalServiceProvider.Value);
+            set => _externalServiceProvider = value;
+        }
+
+        internal static IServiceCollection ServiceCollection
+        {
+            get => _externalServiceCollection;
+            set => _externalServiceCollection = value;
+        }
+    }
+}
---
+++ src/Microsoft.Azure.SignalR.Protocols/ServiceMessage.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.SignalR
+{
+    public enum CommandType
+    {
+        // Service -> SDK
+        Ping = 0,
+        AddConnection,
+        RemoveConnection,
+        AckMessage,
+        // SDK -> Service
+        AddConnectionToGroup,
+        RemoveConnectionFromGroup,
+        SendToConnection,
+        SendToConnections,
+        SendToAll,
+        SendToAllExcept,
+        SendToGroup,
+        SendToGroups,
+        SendToUser,
+        SendToUsers
+    }
+
+    public enum ArgumentType
+    {
+        ConnectionId = 0,
+        Claim,
+        ProtocolName,
+        ConnectionList,
+        GroupName,
+        GroupList,
+        UserId,
+        UserList,
+        ExcludedList
+    }
+
+    public class ServiceMessage
+    {
+        public static readonly string HandshakeProtocol = "json";
+
+        public CommandType Command { get; set; }
+
+        public IDictionary<ArgumentType, string> Arguments { get; set; }
+
+        public byte[] AckPayload { get; set; }
+
+        public IDictionary<string, byte[]> Payloads { get; set; }
+
+        public static readonly ServiceMessage PingMessage = new ServiceMessage
+        {
+            Command = CommandType.Ping
+        };
+    }
+}
