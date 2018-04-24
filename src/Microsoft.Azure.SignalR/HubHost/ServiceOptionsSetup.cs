4643dde03f877ed36e61fe4a5cd8aef6067da7fa
Kevin Zhao <kevinzha@microsoft.com>
2018-04-24 19:46:04 +0800

    provide default value for connection count

--- src/Microsoft.Azure.SignalR/HubHost/ServiceOptionsSetup.cs
+++ src/Microsoft.Azure.SignalR/HubHost/ServiceOptionsSetup.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
@@ -9,8 +8,7 @@ namespace Microsoft.Azure.SignalR
 {
     internal class ServiceOptionsSetup : IConfigureOptions<ServiceOptions>
     {
-        internal static readonly int DefaultConnectionCount = 5;
-        private string _connectionString;
+        private readonly string _connectionString;
 
         public ServiceOptionsSetup(IConfiguration configuration)
         {
@@ -19,10 +17,6 @@ namespace Microsoft.Azure.SignalR
 
         public void Configure(ServiceOptions options)
         {
-            if (options.ConnectionCount == null)
-            {
-                options.ConnectionCount = DefaultConnectionCount;
-            }
             if (options.ConnectionString == null)
             {
                 options.ConnectionString = _connectionString;
