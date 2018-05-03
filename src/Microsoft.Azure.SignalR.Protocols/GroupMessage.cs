23632d92484e494d03932d5c5dcf00532b06d0d3
Kevin Zhao <kevinzha@microsoft.com>
2018-05-03 23:15:58 +0800

    add service protocol test case; add doc comments

--- src/Microsoft.Azure.SignalR.Protocols/GroupMessage.cs
+++ src/Microsoft.Azure.SignalR.Protocols/GroupMessage.cs
@@ -3,11 +3,26 @@
 
 namespace Microsoft.Azure.SignalR.Protocol
 {
+    /// <summary>
+    /// A join-group message.
+    /// </summary>
     public class JoinGroupMessage : ServiceMessage
     {
+        /// <summary>
+        /// Gets or sets the connection Id.
+        /// </summary>
         public string ConnectionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group name.
+        /// </summary>
         public string GroupName { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoinGroupMessage"/> class.
+        /// </summary>
+        /// <param name="connectionId">The connection Id.</param>
+        /// <param name="groupName">The group name, to which the connection will join.</param>
         public JoinGroupMessage(string connectionId, string groupName)
         {
             ConnectionId = connectionId;
@@ -15,11 +30,26 @@ namespace Microsoft.Azure.SignalR.Protocol
         }
     }
 
+    /// <summary>
+    /// A leave-group message.
+    /// </summary>
     public class LeaveGroupMessage : ServiceMessage
     {
+        /// <summary>
+        /// Gets or sets the connection Id.
+        /// </summary>
         public string ConnectionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group name.
+        /// </summary>
         public string GroupName { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeaveGroupMessage"/> class.
+        /// </summary>
+        /// <param name="connectionId">The connection Id.</param>
+        /// <param name="groupName">The group name, from which the connection will leave.</param>
         public LeaveGroupMessage(string connectionId, string groupName)
         {
             ConnectionId = connectionId;
