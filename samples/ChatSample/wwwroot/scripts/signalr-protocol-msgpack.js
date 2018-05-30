a4edc938e234da92fea8e83f51190020d992c3cd
Kevin Zhao <kvn.zhl@gmail.com>
2018-05-30 17:24:53 +0800

    update SignalR dependencies to 1.0.0 (#114)

--- samples/ChatSample/wwwroot/scripts/signalr-protocol-msgpack.js
+++ samples/ChatSample/wwwroot/scripts/signalr-protocol-msgpack.js
@@ -1,4 +1,7 @@
-/* @license
+/** 
+ * @overview ASP.NET Core SignalR JavaScript Client.
+ * @version 1.0.0.
+ * @license
  * Copyright (c) .NET Foundation. All rights reserved.
  * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
  */
@@ -18,6 +21,208 @@ function unwrapExports (x) {
 function createCommonjsModule(fn, module) {
 	return module = { exports: {} }, fn(module, module.exports), module.exports;
 }
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = Object.setPrototypeOf ||
+    ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+    function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var __assign = Object.assign || function __assign(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+    }
+    return t;
+};
+
+function __rest(s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+}
+
+function __decorate(decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+}
+
+function __param(paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+}
+
+function __metadata(metadataKey, metadataValue) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
+}
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
+function __exportStar(m, exports) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+
+function __values(o) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator], i = 0;
+    if (m) return m.call(o);
+    return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+}
+
+function __read(o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+}
+
+function __spread() {
+    for (var ar = [], i = 0; i < arguments.length; i++)
+        ar = ar.concat(__read(arguments[i]));
+    return ar;
+}
+
+function __await(v) {
+    return this instanceof __await ? (this.v = v, this) : new __await(v);
+}
+
+function __asyncGenerator(thisArg, _arguments, generator) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var g = generator.apply(thisArg, _arguments || []), i, q = [];
+    return i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i;
+    function verb(n) { if (g[n]) i[n] = function (v) { return new Promise(function (a, b) { q.push([n, v, a, b]) > 1 || resume(n, v); }); }; }
+    function resume(n, v) { try { step(g[n](v)); } catch (e) { settle(q[0][3], e); } }
+    function step(r) { r.value instanceof __await ? Promise.resolve(r.value.v).then(fulfill, reject) : settle(q[0][2], r);  }
+    function fulfill(value) { resume("next", value); }
+    function reject(value) { resume("throw", value); }
+    function settle(f, v) { if (f(v), q.shift(), q.length) resume(q[0][0], q[0][1]); }
+}
+
+function __asyncDelegator(o) {
+    var i, p;
+    return i = {}, verb("next"), verb("throw", function (e) { throw e; }), verb("return"), i[Symbol.iterator] = function () { return this; }, i;
+    function verb(n, f) { if (o[n]) i[n] = function (v) { return (p = !p) ? { value: __await(o[n](v)), done: n === "return" } : f ? f(v) : v; }; }
+}
+
+function __asyncValues(o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator];
+    return m ? m.call(o) : typeof __values === "function" ? __values(o) : o[Symbol.iterator]();
+}
+
+function __makeTemplateObject(cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+}
+
+function __importStar(mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result.default = mod;
+    return result;
+}
+
+function __importDefault(mod) {
+    return (mod && mod.__esModule) ? mod : { default: mod };
+}
+
+
+var tslib_es6 = Object.freeze({
+	__extends: __extends,
+	__assign: __assign,
+	__rest: __rest,
+	__decorate: __decorate,
+	__param: __param,
+	__metadata: __metadata,
+	__awaiter: __awaiter,
+	__generator: __generator,
+	__exportStar: __exportStar,
+	__values: __values,
+	__read: __read,
+	__spread: __spread,
+	__await: __await,
+	__asyncGenerator: __asyncGenerator,
+	__asyncDelegator: __asyncDelegator,
+	__asyncValues: __asyncValues,
+	__makeTemplateObject: __makeTemplateObject,
+	__importStar: __importStar,
+	__importDefault: __importDefault
+});
 
 var byteLength_1 = byteLength;
 var toByteArray_1 = toByteArray;
@@ -1947,6 +2152,7 @@ var buffer_4 = buffer.kMaxLength;
 
 var BinaryMessageFormat_1 = createCommonjsModule(function (module, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
+// Not exported from index.
 var BinaryMessageFormat = /** @class */ (function () {
     function BinaryMessageFormat() {
     }
@@ -2021,18 +2227,50 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 
 
+// TypeDoc's @inheritDoc and @link don't work across modules :(
+/** Implements the MessagePack Hub Protocol */
 var MessagePackHubProtocol = /** @class */ (function () {
     function MessagePackHubProtocol() {
+        /** The name of the protocol. This is used by SignalR to resolve the protocol between the client and server. */
         this.name = "messagepack";
+        /** The version of the protocol. */
         this.version = 1;
+        /** The TransferFormat of the protocol. */
         this.transferFormat = signalr.TransferFormat.Binary;
     }
+    /** Creates an array of HubMessage objects from the specified serialized representation.
+     *
+     * @param {ArrayBuffer} input An ArrayBuffer containing the serialized representation.
+     * @param {ILogger} logger A logger that will be used to log messages that occur during parsing.
+     */
     MessagePackHubProtocol.prototype.parseMessages = function (input, logger) {
         var _this = this;
+        // The interface does allow "string" to be passed in, but this implementation does not. So let's throw a useful error.
+        if (!(input instanceof ArrayBuffer)) {
+            throw new Error("Invalid input for MessagePack hub protocol. Expected an ArrayBuffer.");
+        }
         if (logger === null) {
             logger = signalr.NullLogger.instance;
         }
         return BinaryMessageFormat_1.BinaryMessageFormat.parse(input).map(function (m) { return _this.parseMessage(m, logger); });
+    };
+    /** Writes the specified HubMessage to an ArrayBuffer and returns it.
+     *
+     * @param {HubMessage} message The message to write.
+     * @returns {ArrayBuffer} An ArrayBuffer containing the serialized representation of the message.
+     */
+    MessagePackHubProtocol.prototype.writeMessage = function (message) {
+        switch (message.type) {
+            case signalr.MessageType.Invocation:
+                return this.writeInvocation(message);
+            case signalr.MessageType.StreamInvocation:
+                return this.writeStreamInvocation(message);
+            case signalr.MessageType.StreamItem:
+            case signalr.MessageType.Completion:
+                throw new Error("Writing messages of type '" + message.type + "' is not supported.");
+            default:
+                throw new Error("Invalid message type.");
+        }
     };
     MessagePackHubProtocol.prototype.parseMessage = function (input, logger) {
         if (input.length === 0) {
@@ -2045,15 +2283,15 @@ var MessagePackHubProtocol = /** @class */ (function () {
         }
         var messageType = properties[0];
         switch (messageType) {
-            case 1 /* Invocation */:
+            case signalr.MessageType.Invocation:
                 return this.createInvocationMessage(this.readHeaders(properties), properties);
-            case 2 /* StreamItem */:
+            case signalr.MessageType.StreamItem:
                 return this.createStreamItemMessage(this.readHeaders(properties), properties);
-            case 3 /* Completion */:
+            case signalr.MessageType.Completion:
                 return this.createCompletionMessage(this.readHeaders(properties), properties);
-            case 6 /* Ping */:
+            case signalr.MessageType.Ping:
                 return this.createPingMessage(properties);
-            case 7 /* Close */:
+            case signalr.MessageType.Close:
                 return this.createCloseMessage(properties);
             default:
                 // Future protocol changes can add message types, old clients can ignore them
@@ -2069,7 +2307,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
         return {
             // Close messages have no headers.
             error: properties[1],
-            type: 7 /* Close */,
+            type: signalr.MessageType.Close,
         };
     };
     MessagePackHubProtocol.prototype.createPingMessage = function (properties) {
@@ -2079,7 +2317,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
         }
         return {
             // Ping messages have no headers.
-            type: 6 /* Ping */,
+            type: signalr.MessageType.Ping,
         };
     };
     MessagePackHubProtocol.prototype.createInvocationMessage = function (headers, properties) {
@@ -2094,7 +2332,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
                 headers: headers,
                 invocationId: invocationId,
                 target: properties[3],
-                type: 1 /* Invocation */,
+                type: signalr.MessageType.Invocation,
             };
         }
         else {
@@ -2102,7 +2340,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
                 arguments: properties[4],
                 headers: headers,
                 target: properties[3],
-                type: 1 /* Invocation */,
+                type: signalr.MessageType.Invocation,
             };
         }
     };
@@ -2115,7 +2353,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
             headers: headers,
             invocationId: properties[2],
             item: properties[3],
-            type: 2 /* StreamItem */,
+            type: signalr.MessageType.StreamItem,
         };
     };
     MessagePackHubProtocol.prototype.createCompletionMessage = function (headers, properties) {
@@ -2135,7 +2373,7 @@ var MessagePackHubProtocol = /** @class */ (function () {
             headers: headers,
             invocationId: properties[2],
             result: null,
-            type: 3 /* Completion */,
+            type: signalr.MessageType.Completion,
         };
         switch (resultKind) {
             case errorResult:
@@ -2147,28 +2385,15 @@ var MessagePackHubProtocol = /** @class */ (function () {
         }
         return completionMessage;
     };
-    MessagePackHubProtocol.prototype.writeMessage = function (message) {
-        switch (message.type) {
-            case 1 /* Invocation */:
-                return this.writeInvocation(message);
-            case 4 /* StreamInvocation */:
-                return this.writeStreamInvocation(message);
-            case 2 /* StreamItem */:
-            case 3 /* Completion */:
-                throw new Error("Writing messages of type '" + message.type + "' is not supported.");
-            default:
-                throw new Error("Invalid message type.");
-        }
-    };
     MessagePackHubProtocol.prototype.writeInvocation = function (invocationMessage) {
         var msgpack = msgpack5();
-        var payload = msgpack.encode([1 /* Invocation */, invocationMessage.headers || {}, invocationMessage.invocationId || null,
+        var payload = msgpack.encode([signalr.MessageType.Invocation, invocationMessage.headers || {}, invocationMessage.invocationId || null,
             invocationMessage.target, invocationMessage.arguments]);
         return BinaryMessageFormat_1.BinaryMessageFormat.write(payload.slice());
     };
     MessagePackHubProtocol.prototype.writeStreamInvocation = function (streamInvocationMessage) {
         var msgpack = msgpack5();
-        var payload = msgpack.encode([4 /* StreamInvocation */, streamInvocationMessage.headers || {}, streamInvocationMessage.invocationId,
+        var payload = msgpack.encode([signalr.MessageType.StreamInvocation, streamInvocationMessage.headers || {}, streamInvocationMessage.invocationId,
             streamInvocationMessage.target, streamInvocationMessage.arguments]);
         return BinaryMessageFormat_1.BinaryMessageFormat.write(payload.slice());
     };
@@ -2190,21 +2415,23 @@ var MessagePackHubProtocol_2 = MessagePackHubProtocol_1.MessagePackHubProtocol;
 
 var cjs = createCommonjsModule(function (module, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
+// Version token that will be replaced by the prepack command
+/** The version of the SignalR Message Pack protocol library. */
+exports.VERSION = "0.0.0-DEV_BUILD";
 
 exports.MessagePackHubProtocol = MessagePackHubProtocol_1.MessagePackHubProtocol;
 
 });
 
 unwrapExports(cjs);
-var cjs_1 = cjs.MessagePackHubProtocol;
+var cjs_1 = cjs.VERSION;
+var cjs_2 = cjs.MessagePackHubProtocol;
 
 var browserIndex = createCommonjsModule(function (module, exports) {
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 Object.defineProperty(exports, "__esModule", { value: true });
+
 // This is where we add any polyfills we'll need for the browser. It is the entry module for browser-specific builds.
-__export(cjs);
+tslib_es6.__exportStar(cjs, exports);
 
 });
 
