11ba855b094d612c0e363f1d1e691829d1e532b1
Liangying Wei <lianwei@microsoft.com>
2018-07-19 12:42:19 +0800

    Add Microsoft.Azure.AspNet.SignalR.csproj and SDK skeleton

---
+++ src/Microsoft.Azure.AspNet.SignalR/OwinExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNet.SignalR;
+using System;
+
+namespace Owin
+{
+    public static partial class OwinExtensions
+    {
+        public static IAppBuilder MapAzureSignalR(this IAppBuilder builder)
+        {
+            return builder.MapAzureSignalR(new HubConfiguration());
+        }
+
+        public static IAppBuilder MapAzureSignalR(this IAppBuilder builder, HubConfiguration configuration)
+        {
+            return builder.MapAzureSignalR("/signalr", configuration);
+        }
+
+        public static IAppBuilder MapAzureSignalR(this IAppBuilder builder, string path, HubConfiguration configuration)
+        {
+            return builder.Map(path, subApp => subApp.RunAzureSignalR(configuration));
+        }
+
+        public static void RunAzureSignalR(this IAppBuilder builder)
+        {
+            builder.RunAzureSignalR(new HubConfiguration());
+        }
+
+        public static void RunAzureSignalR(this IAppBuilder builder, HubConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
