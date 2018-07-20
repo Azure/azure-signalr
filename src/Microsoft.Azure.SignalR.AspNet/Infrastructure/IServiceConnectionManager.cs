fc967e5d0862e282c8e4ac76a0a116b9a8dbc552
Liangying Wei <lianwei@microsoft.com>
2018-07-20 15:50:21 +0800

    Implement RunAzureSignalR in the API, add necessory class skeleton
    Resolve comments, and make ServiceOptions configurable
    
    Add test project and RunAzureSignalR test
    
    fix format: using System directives should be first
    
    Add 2 to-dos

---
+++ src/Microsoft.Azure.SignalR.AspNet/Infrastructure/IServiceConnectionManager.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.SignalR.AspNet
+{
+    internal interface IServiceConnectionManager : IServiceConnection
+    {
+        void AddConnection(string hubName, IServiceConnection connection);
+
+        IServiceConnection WithHub(string hubName);
+    }
+}
---
+++ src/Microsoft.Azure.SignalR.AspNet/Infrastructure/ServiceConnectionManager.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.SignalR.Protocol;
+
+namespace Microsoft.Azure.SignalR.AspNet
+{
+    internal class ServiceConnectionManager : IServiceConnectionManager
+    {
+        public void AddConnection(string hubName, IServiceConnection connection)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StartAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IServiceConnection WithHub(string hubName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task WriteAsync(ServiceMessage serviceMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task WriteAsync(string connectionId, object message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
