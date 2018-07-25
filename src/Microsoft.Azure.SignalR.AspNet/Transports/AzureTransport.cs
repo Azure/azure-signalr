4e55ac4a7ff81e7430e4f7fb6fe06eaefb937f3a
Liangying Wei <lianwei@microsoft.com>
2018-07-25 14:27:37 +0800

    fix format: using System directives should be first

--- src/Microsoft.Azure.SignalR.AspNet/Transports/AzureTransport.cs
+++ src/Microsoft.Azure.SignalR.AspNet/Transports/AzureTransport.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.AspNet.SignalR.Hosting;
-using Microsoft.AspNet.SignalR.Transports;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hosting;
+using Microsoft.AspNet.SignalR.Transports;
 
 namespace Microsoft.Azure.SignalR.AspNet
 {
