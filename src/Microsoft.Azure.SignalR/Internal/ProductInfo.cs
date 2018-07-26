672e24423d723aef28efcccbe006ef7ff2124c93
Jialin Xin <jixin@microsoft.com>
2018-07-26 10:27:31 +0800

    add null check for safety

--- src/Microsoft.Azure.SignalR/Internal/ProductInfo.cs
+++ src/Microsoft.Azure.SignalR/Internal/ProductInfo.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -15,8 +13,8 @@ namespace Microsoft.Azure.SignalR
             var assembly = typeof(ProductInfo).GetTypeInfo().Assembly;
             var packageId = assembly.GetName().Name;
             var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-            var runtime = RuntimeInformation.FrameworkDescription.Trim();
-            var operatingSystem = RuntimeInformation.OSDescription.Trim();
+            var runtime = RuntimeInformation.FrameworkDescription?.Trim();
+            var operatingSystem = RuntimeInformation.OSDescription?.Trim();
             var processorArchitecture = RuntimeInformation.ProcessArchitecture.ToString().Trim();
             
             return $"{packageId}/{version} ({runtime}; {operatingSystem}; {processorArchitecture})";
