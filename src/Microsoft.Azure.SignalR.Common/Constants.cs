1536d7fbbfacee4eeddad82257fe9832d5e131d2
Liangying Wei <lianwei@microsoft.com>
2018-08-16 11:32:33 +0800

    Add Constants to Common project

---
+++ src/Microsoft.Azure.SignalR.Common/Constants.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Azure.SignalR
+{
+    internal static class Constants
+    {
+        public static class ClaimType
+        {
+            public const string AzureSignalRSysPrefix = "azure.signalr.sys.";
+            public const string AuthenticationType = AzureSignalRSysPrefix + "authenticationtype";
+            public const string UserId = AzureSignalRSysPrefix + "userid";
+        }
+
+        public static class Path
+        {
+            public const string Negotiate = "/negotiate";
+        }
+
+        public static class Config
+        {
+            public static readonly string ConnectionStringKey = "Azure:SignalR:ConnectionString";
+        }
+    }
+}
