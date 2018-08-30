9eafd6dbd268eca36cd5b2db6993c55dbf2ff584
Liangying Wei <lianwei@microsoft.com>
2018-08-30 17:46:58 +0800

    Add a simple sample instead

---
+++ samples/ChatSample.AspNet/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNet.SignalR;
+
+namespace ChatSample.AspNet
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
--- samples/ChatSample.AspNet/Startup.cs
+++ samples/ChatSample.AspNet/Startup.cs
@@ -1,13 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Diagnostics;
-using System.Web.Http;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(ChatSample.AspNet.Startup))]
@@ -21,60 +17,7 @@ namespace ChatSample.AspNet
             // Turn tracing on programmatically
             GlobalHost.TraceManager.Switch.Level = SourceLevels.Information;
 
-            ConfigureAuth(app);
-
-            app
-                .Map("/api", map =>
-                {
-                    HttpConfiguration config = new HttpConfiguration();
-                    config.MapHttpAttributeRoutes();
-                    map.UseWebApi(config);
-                })
-                .MapSignalR();
-        }
-
-        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
-        public void ConfigureAuth(IAppBuilder app)
-        {
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/api/cookie/login"),
-                CookieName = typeof(Startup).FullName,
-                Provider = new CookieAuthenticationProvider
-                {
-                    OnApplyRedirect = ctx =>
-                    {
-                        if (!IsAjaxRequest(ctx.Request))
-                        {
-                            ctx.Response.Redirect(ctx.RedirectUri);
-                        }
-                    }
-                }
-            });
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-        }
-
-        private static bool IsAjaxRequest(IOwinRequest request)
-        {
-            IReadableStringCollection query = request.Query;
-            if ((query != null) && (query["X-Requested-With"] == "XMLHttpRequest"))
-            {
-                return true;
-            }
-            IHeaderDictionary headers = request.Headers;
-            return ((headers != null) && (headers["X-Requested-With"] == "XMLHttpRequest"));
+            app.RunSignalR();
         }
     }
 }
