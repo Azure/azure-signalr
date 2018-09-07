626aa61e981918b88cd654a2695d8f40c18c0ade
Liangying.Wei <lianwei@microsoft.com>
2018-09-07 16:37:47 +0800

    [ASP.NET SignalR]Add a sample project for old signalr (#183)
    * Add a sample project for old signalr
    
    * Add a simple sample instead

---
+++ samples/ChatSample.AspNet/Startup.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatSample.AspNet.Startup))]
+
+namespace ChatSample.AspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Turn tracing on programmatically
+            GlobalHost.TraceManager.Switch.Level = SourceLevels.Information;
+
+            app.RunSignalR();
+        }
+    }
+}
