807ec40a6fdf80798037d3916aca61fa43bb0492
Liangying Wei <lianwei@microsoft.com>
2018-11-23 16:57:32 +0800

    Add aspnet selfhost samples and update aspnet sdk to 2.4.0

---
+++ samples/AspNet.ChatSample/AspNet.ChatSample.SelfHostServer/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using Microsoft.AspNet.SignalR;
+
+namespace AspNet.ChatSample.SelfHostServer
+{
+    public class ChatHub : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.roadcastMessage("broadcastMessage", name, message);
+            Trace.TraceInformation("Broadcasting...");
+        }
+
+        public void Echo(string message)
+        {
+            Clients.Client(Context.ConnectionId).Echo($"{message} (echo from server, Client IP: {GetIpAddress()})");
+            Trace.TraceInformation("Echo...");
+        }
+
+        private string GetIpAddress()
+        {
+            object address = null;
+            Context.Request.Environment?.TryGetValue("server.RemoteIpAddress", out address);
+            return address as string;
+        }
+    }
+}
---
+++ samples/AspNet.ChatSample/AspNet.ChatSample.SelfHostServer/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Owin.Hosting;
+
+namespace AspNet.ChatSample.SelfHostServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8009"))
+            {
+                Console.WriteLine("Server running at http://localhost:8009/");
+                Console.ReadLine();
+            }
+        }
+    }
+}
