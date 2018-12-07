378287462cfad6d01b7cbfc90e98d6e2e53a4ae7
Wanpeng Li <wanl@microsoft.com>
2018-12-07 16:49:47 +0800

    Add project and interfaces

---
+++ src/Microsoft.Azure.SignalR.SignalRService/ServiceManagerBuilder.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.SignalR.SignalRService
+{
+    public class ServiceManagerBuilder
+    {
+        public ServiceManagerBuilder WithCredential(string conenctionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceManagerBuilder WithCredential(string endpoint, string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceManagerBuilder WithCredential(string endpoint, IList<string> accessTokens)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceManagerBuilder WithOptions(Action<ServiceUtilityOptions> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceHubContext CreateHubContextAsync(string hubName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceManager Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
