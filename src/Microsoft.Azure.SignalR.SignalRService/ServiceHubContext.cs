b511c8091ed386289211a809d8a2202d692d1137
Wanpeng Li <wanl@microsoft.com>
2018-12-17 13:41:54 +0800

    Remove unnecessary libraries

--- src/Microsoft.Azure.SignalR.SignalRService/ServiceHubContext.cs
+++ src/Microsoft.Azure.SignalR.SignalRService/ServiceHubContext.cs
@@ -1,10 +1,8 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.SignalR.SignalRService
