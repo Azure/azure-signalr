baec7eb4c86334369301b567df41e139ef9b067e
Wanpeng Li <wanl@microsoft.com>
2018-12-18 13:04:06 +0800

    change using order

--- src/Microsoft.Azure.SignalRService/ServiceHubContext.cs
+++ src/Microsoft.Azure.SignalRService/ServiceHubContext.cs
@@ -1,9 +1,9 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Microsoft.Azure.SignalRService
 {
