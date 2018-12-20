fa3a2de1ccb452239a8d93e1f8dc8d594fe36953
Wanpeng Li <wanl@microsoft.com>
2018-12-20 10:48:44 +0800

    Only generate client access token

--- src/Microsoft.Azure.SignalRService/ServiceManager.cs
+++ src/Microsoft.Azure.SignalRService/ServiceManager.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.SignalRService
@@ -13,7 +15,7 @@ namespace Microsoft.Azure.SignalRService
             throw new NotImplementedException();
         }
 
-        public string GenerateAccessToken(Scope scope, string hubName, string connectionId = null, string userId = null, string groupName = null, TimeSpan? lifeTime = null)
+        public string GenerateClientAccessToken(IList<Claim> claims, TimeSpan? lifeTime = null)
         {
             throw new NotImplementedException();
         }
