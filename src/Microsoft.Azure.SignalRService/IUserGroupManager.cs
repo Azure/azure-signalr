6730bbf64a06ad38a0bcefe55a2d3b1feabfcd31
Wanpeng Li <38236089+realwanpengli@users.noreply.github.com>
2018-12-24 15:00:59 +0800

    Add project and interfaces for SignalR Service SDK (#289)
    * Add project and interfaces
    
    * Change parameter name for IUserGroupManager
    
    * Remove unnecessary interface
    
    * Remove unnecessary libraries
    
    * stage file
    
    * typo
    
    * Make it async
    
    * Change name
    
    * Modify interface for GenerateAccessToken
    
    * Remove unnecessary package
    
    * Change package name
    
    * Update sln
    
    * Change namespace
    
    * Change comment style
    
    * change using order
    
    * typo
    
    * add interface for ServiceHubContext
    
    * do not package for now
    
    * Remove take access token as input
    
    * Change name from ServiceHubContextBackend to ServiceTransportType
    
    * Only generate client access token
    
    * Make interface
    
    * Comment
    
    * COMMENT UPDATE

---
+++ src/Microsoft.Azure.SignalRService/IUserGroupManager.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.SignalRService
+{
+    public interface IUserGroupManager
+    {
+        Task AddToGroupAsync(string userId, string groupName, CancellationToken cancellationToken = default);
+        Task RemoveFromGroupAsync(string userId, string groupName, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Microsoft.Azure.SignalRService/ServiceHubContext.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalRService
+{
+    internal class ServiceHubContext : IServiceHubContext
+    {
+        public IUserGroupManager UserGroups => throw new NotImplementedException();
+
+        public IHubClients Clients => throw new NotImplementedException();
+
+        public IGroupManager Groups => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
