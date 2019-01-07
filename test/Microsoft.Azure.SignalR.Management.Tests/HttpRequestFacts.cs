5e10ffcecc71b05ecc8a97c29fa474304d6eaa1d
Wanpeng Li <wanl@microsoft.com>
2019-01-07 16:38:08 +0800

    Add unit test

---
+++ test/Microsoft.Azure.SignalR.Management.Tests/HttpRequestFacts.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.SignalR.Management.Tests
+{
+    public class HttpRequestFacts
+    {
+        [Fact]
+        public async Task RequestAndResponse()
+        {
+            var httpRequest = new HttpRequest();
+            var result = await httpRequest.SendAsync("http://baidu.com", null, null, HttpMethod.Get);
+
+            Assert.Equal($"{HttpStatusCode.OK}", $"{result.StatusCode}");
+        }
+    }
+}
