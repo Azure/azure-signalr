3ba4108b98a3b2be4802f81048c05123030225d0
Liangying Wei <lianwei@microsoft.com>
2019-01-07 16:53:24 +0800

    Add router to negotiate and use manager to manage endpoints

---
+++ src/Microsoft.Azure.SignalR.Common/Endpoints/DefaultRouter.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.SignalR
+{
+    internal class DefaultRouter : IEndpointRouter
+    {
+        public ServiceEndpoint GetNegotiateEndpoint(IReadOnlyList<ServiceEndpoint> primaryEndpoints)
+        {
+            // get primary endpoints snapshot
+            return primaryEndpoints[StaticRandom.Next(primaryEndpoints.Count)];
+        }
+    }
+}
---
+++ src/Microsoft.Azure.SignalR.Common/Endpoints/EndpointType.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace Microsoft.Azure.SignalR
+{
+    /// <summary>
+    /// TODO: make it public so that user can set/update endpoint type
+    /// </summary>
+    internal enum EndpointType
+    {
+        Primary,
+        Secondary
+    }
+}
