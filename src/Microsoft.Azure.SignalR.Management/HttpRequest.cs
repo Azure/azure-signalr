c01f17ff04375c8815776285140c0611dd36fbb5
Wanpeng Li <wanl@microsoft.com>
2019-01-07 16:21:46 +0800

    Http request class

---
+++ src/Microsoft.Azure.SignalR.Management/HttpRequest.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.SignalR.Management
+{
+    internal class HttpRequest
+    {
+        private static readonly IHttpClientFactory _clientFactory = null;
+
+        static HttpRequest()
+        {
+            if (_clientFactory == null)
+            {
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.AddHttpClient();
+                var services = serviceCollection.BuildServiceProvider();
+
+                _clientFactory = services.GetRequiredService<IHttpClientFactory>();
+            }
+        }
+
+        public Task<HttpResponseMessage> SendAsync(string url, PayloadMessage payload, string tokenString, HttpMethod httpMethod)
+        {
+            var request = new HttpRequestMessage(httpMethod, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+            return _clientFactory.CreateClient().SendAsync(request);
+        }
+    }
+}
