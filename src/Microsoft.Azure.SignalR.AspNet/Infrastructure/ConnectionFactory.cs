95c9e72744a2c0a94fa9e9ecae85f1ccbfdf504d
Liangying Wei <lianwei@microsoft.com>
2019-01-09 13:10:16 +0800

    Move ConnectionFactory to common and to be shared by 2 signalr

--- src/Microsoft.Azure.SignalR.AspNet/Infrastructure/ConnectionFactory.cs
+++ src/Microsoft.Azure.SignalR.AspNet/Infrastructure/ConnectionFactory.cs
@@ -3,12 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
-using Microsoft.AspNetCore.Connections;
-using Microsoft.AspNetCore.Http.Connections;
-using Microsoft.AspNetCore.Http.Connections.Client;
 using Microsoft.Azure.SignalR.Common;
 using Microsoft.Azure.SignalR.Protocol;
 using Microsoft.Extensions.Logging;
@@ -19,7 +15,7 @@ namespace Microsoft.Azure.SignalR.AspNet
     /// <summary>
     /// TODO: This factory class is responsible for creating, disposing and starting the server-service connections
     /// </summary>
-    internal class ConnectionFactory : IConnectionFactory
+    internal class ConnectionFactory
     {
         private readonly ServiceOptions _options;
         private readonly HubConfiguration _config;
@@ -31,7 +27,6 @@ namespace Microsoft.Azure.SignalR.AspNet
         private readonly IServiceProtocol _protocol;
         private readonly IServiceEndpointManager _serviceEndpointManager;
         private readonly string _name;
-        private readonly string _userId;
         
         // Align the header key with ASP.Net core.
         private static Dictionary<string, string> CustomHeader = new Dictionary<string, string> { { "Asrs-User-Agent", ProductInfo.GetProductInfo() } };
@@ -44,7 +39,6 @@ namespace Microsoft.Azure.SignalR.AspNet
             _config = hubConfig;
             _hubNames = hubNames;
             _name = $"{nameof(ConnectionFactory)}[{string.Join(",", hubNames)}]";
-            _userId = GenerateServerName();
             _loggerFactory = loggerFactory;
             _protocol = protocol ?? throw new ArgumentNullException(nameof(protocol));
             _serviceConnectionManager = serviceConnectionManager ?? throw new ArgumentNullException(nameof(serviceConnectionManager));
@@ -54,7 +48,7 @@ namespace Microsoft.Azure.SignalR.AspNet
             _logger = _loggerFactory.CreateLogger<ConnectionFactory>();
         }
 
-        public async Task<ConnectionContext> ConnectAsync(TransferFormat transferFormat, string connectionId, string hubName, CancellationToken cancellationToken = default)
+        public Task StartAsync()
         {
             var endpoints = _serviceEndpointManager.GetAvailableEndpoints();
             if (endpoints.Count == 0)
@@ -65,68 +59,17 @@ namespace Microsoft.Azure.SignalR.AspNet
             // TODO: support multiple endpoints
             var provider = _serviceEndpointManager.GetEndpointProvider(endpoints[0]);
 
-            var httpConnectionOptions = new HttpConnectionOptions
-            {
-                Url = GetServiceUrl(connectionId, hubName, provider),
-                AccessTokenProvider = () => Task.FromResult(provider.GenerateServerAccessToken(hubName, _userId)),
-                Transports = HttpTransportType.WebSockets,
-                SkipNegotiation = true,
-                Headers = CustomHeader
-            };
-            var httpConnection = new HttpConnection(httpConnectionOptions, _loggerFactory);
-            try
-            {
-                await httpConnection.StartAsync(transferFormat);
-                return httpConnection;
-            }
-            catch
-            {
-                await httpConnection.DisposeAsync();
-                throw;
-            }
-        }
-
-        public Task DisposeAsync(ConnectionContext connection)
-        {
-            if (connection == null)
-            {
-                return Task.CompletedTask;
-            }
-            
-            return ((HttpConnection)connection).DisposeAsync();
-        }
-
-        public Task StartAsync()
-        {
             _serviceConnectionManager.Initialize(
-                hub => new ServiceConnection(hub, Guid.NewGuid().ToString(), _protocol, this, _clientConnectionManager, _logger),
+                hub =>
+                {
+                    var connectionFactory = new ServiceConnectionFactory(hub, provider, _loggerFactory);
+                    return new ServiceConnection(hub, Guid.NewGuid().ToString(), _protocol, connectionFactory, _clientConnectionManager, _logger);
+                },
                 _options.ConnectionCount);
 
             Log.StartingConnection(_logger, _name, _options.ConnectionCount, _hubNames.Count);
 
             return _serviceConnectionManager.StartAsync();
-        }
-
-        private Uri GetServiceUrl(string connectionId, string hubName, IServiceEndpointProvider provider)
-        {
-            var baseUri = new UriBuilder(provider.GetServerEndpoint(hubName));
-            var query = "cid=" + connectionId;
-            if (baseUri.Query != null && baseUri.Query.Length > 1)
-            {
-                baseUri.Query = baseUri.Query.Substring(1) + "&" + query;
-            }
-            else
-            {
-                baseUri.Query = query;
-            }
-            return baseUri.Uri;
-        }
-
-        private static string GenerateServerName()
-        {
-            // Use the machine name for convenient diagnostics, but add a guid to make it unique.
-            // Example: MyServerName_02db60e5fab243b890a847fa5c4dcb29
-            return $"{Environment.MachineName}_{Guid.NewGuid():N}";
         }
 
         private static class Log
