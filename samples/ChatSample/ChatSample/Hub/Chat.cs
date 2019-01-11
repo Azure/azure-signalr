e0ed2d7ce4045efb21700bad4c6a83568cfb60c3
Liangying Wei <lianwei@microsoft.com>
2019-01-11 15:50:53 +0800

    Add c# client to aspnet core ChatSample

--- samples/ChatSample/ChatSample/Hub/Chat.cs
+++ samples/ChatSample/ChatSample/Hub/Chat.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -11,11 +12,13 @@ namespace ChatSample
         public void BroadcastMessage(string name, string message)
         {
             Clients.All.SendAsync("broadcastMessage", name, message);
+            Console.WriteLine("Broadcasting...");
         }
 
         public void Echo(string name, string message)
         {
             Clients.Client(Context.ConnectionId).SendAsync("echo", name, $"{message} (echo from server, Client IP: {Context.GetHttpContext().Connection.RemoteIpAddress})");
+            Console.WriteLine("Echo...");
         }
     }
 }
