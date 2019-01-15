5b12b367fe775bd044a2c6c4b633ccbc26095910
Wanpeng Li <38236089+realwanpengli@users.noreply.github.com>
2019-01-15 13:30:13 +0800

    Management SDK: REST API provider (#327)

---
+++ src/Microsoft.Azure.SignalR.Management/RestApiEndpoint.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.SignalR.Management
+{
+    internal class RestApiEndpoint
+    {
+        public string Audience { get; }
+        public string Token { get; }
+
+        public RestApiEndpoint(string endpoint, string token)
+        {
+            Audience = endpoint;
+            Token = token;
+        }
+    }
+}
