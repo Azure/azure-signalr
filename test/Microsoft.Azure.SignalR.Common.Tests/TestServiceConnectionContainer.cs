3ab0f0d385ac1afe9c5f8fa177e3424086463f4c
Chenyang Liu <chenyl@microsoft.com>
2019-02-01 09:27:54 +0800

    Fix tests

--- test/Microsoft.Azure.SignalR.Common.Tests/TestServiceConnectionContainer.cs
+++ test/Microsoft.Azure.SignalR.Common.Tests/TestServiceConnectionContainer.cs
@@ -2,14 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.SignalR.Tests
 {
     internal sealed class TestServiceConnectionContainer : ServiceConnectionContainerBase
     {
-        public TestServiceConnectionContainer(List<IServiceConnection> serviceConnections, ServiceEndpoint endpoint = null) : base(null, null, serviceConnections, endpoint)
+        public TestServiceConnectionContainer(List<IServiceConnection> serviceConnections, ServiceEndpoint endpoint = null) : base(null, null, new ConcurrentDictionary<int?, IServiceConnection>(serviceConnections.Select((c, i) => new {c ,i}).ToDictionary(x => (int?)x.i, x => x.c)), endpoint)
         {
         }
 
