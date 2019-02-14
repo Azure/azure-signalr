ee2a797d2dbef1afe63ff676427fffc591878a23
Wanpeng Li <wanl@microsoft.com>
2019-02-14 16:54:55 +0800

    add doc for users

--- src/Microsoft.Azure.SignalR.Management/IServiceManager.cs
+++ src/Microsoft.Azure.SignalR.Management/IServiceManager.cs
@@ -8,12 +8,33 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.SignalR.Management
 {
+    /// <summary>
+    /// A manager abstraction for managing Azure SignalR Service.
+    /// </summary>
     public interface IServiceManager
     {
+        /// <summary>
+        /// Creates an instance of <see cref="IServiceHubContext"/> asynchronously.
+        /// </summary>
+        /// <param name="hubName">The hub name.</param>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task<IServiceHubContext> CreateHubContextAsync(string hubName);
 
+        /// <summary>
+        /// Creates a client access token for SignalR hub connections to connect to Azure SignalR Service.
+        /// </summary>
+        /// <param name="hubName">The hub name.</param>
+        /// <param name="userId">The user ID.</param>
+        /// <param name="claims">The claim list to be put into access token.</param>
+        /// <param name="lifeTime">The lifetime of the token. The default value is one hour.</param>
+        /// <returns>Client access token to Azure SignalR Service.</returns>
         string GenerateClientAccessToken(string hubName, string userId = null, IList<Claim> claims = null, TimeSpan? lifeTime = null);
 
+        /// <summary>
+        /// Creates an client endpoint for SignalR hub connections to connect to Azure SignalR Service
+        /// </summary>
+        /// <param name="hubName">The hub name.</param>
+        /// <returns>Client endpoint to Azure SignalR Service.</returns>
         string GetClientEndpoint(string hubName);
     }
 }
