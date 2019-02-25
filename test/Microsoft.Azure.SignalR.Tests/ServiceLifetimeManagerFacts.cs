17caac2a5b4f8f2ba0330bf2536319cec728558d
Wanpeng Li <wanl@microsoft.com>
2019-02-25 17:56:42 +0800

    update

--- test/Microsoft.Azure.SignalR.Tests/ServiceLifetimeManagerFacts.cs
+++ test/Microsoft.Azure.SignalR.Tests/ServiceLifetimeManagerFacts.cs
@@ -97,7 +97,7 @@ namespace Microsoft.Azure.SignalR.Tests
             var proxy = new ServiceConnectionProxy();
 
             var serviceConnectionManager = new ServiceConnectionManager<TestHub>();
-            serviceConnectionManager.ServiceConnection = proxy.ServiceConnectionContainer;
+            serviceConnectionManager.SetServiceConnection(proxy.ServiceConnectionContainer);
 
             var serviceLifetimeManager = new ServiceLifetimeManager<TestHub>(serviceConnectionManager,
                 proxy.ClientConnectionManager, HubProtocolResolver, Logger, Marker);
