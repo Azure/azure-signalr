43552d79220d7872337e82f2b5c68fbc21ec7291
Wanpeng Li <wanl@microsoft.com>
2019-02-25 18:09:17 +0800

    remove package not in use

--- src/Microsoft.Azure.SignalR/DependencyInjectionExtensions.cs
+++ src/Microsoft.Azure.SignalR/DependencyInjectionExtensions.cs
@@ -4,7 +4,6 @@
 using System;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Azure.SignalR;
-using Microsoft.Azure.SignalR.Common.ServiceConnections;
 using Microsoft.Azure.SignalR.Protocol;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
