a17a65c944a3bfa094e198280de231724656bf6b
Wanpeng Li <38236089+realwanpengli@users.noreply.github.com>
2019-03-06 15:38:17 +0800

    Remove exception in HandlePingAsync (#426)

--- src/Microsoft.Azure.SignalR.Common/ServiceConnections/WeakServiceConnectionContainer.cs
+++ src/Microsoft.Azure.SignalR.Common/ServiceConnections/WeakServiceConnectionContainer.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.SignalR.Protocol;
 
@@ -31,7 +29,7 @@ namespace Microsoft.Azure.SignalR.Common.ServiceConnections
 
         public override Task HandlePingAsync(PingMessage pingMessage)
         {
-            throw new NotSupportedException();
+            return Task.CompletedTask;
         }
     }
 }
