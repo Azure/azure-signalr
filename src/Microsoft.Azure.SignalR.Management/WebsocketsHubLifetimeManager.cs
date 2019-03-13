e4f070efd9dc37b41d8844e734c4f3a803d02267
Wanpeng Li <wanl@microsoft.com>
2019-03-13 15:38:48 +0800

    user group add/remove

--- src/Microsoft.Azure.SignalR.Management/WebsocketsHubLifetimeManager.cs
+++ src/Microsoft.Azure.SignalR.Management/WebsocketsHubLifetimeManager.cs
@@ -5,23 +5,48 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Azure.SignalR.Protocol;
 
 namespace Microsoft.Azure.SignalR.Management
 {
     internal class WebSocketsHubLifetimeManager<THub> : ServiceLifetimeManagerBase<THub>, IHubLifetimeManagerForUserGroup where THub : Hub
     {
+        private const string NullOrEmptyStringErrorMessage = "Argument cannot be null or empty.";
+
         public WebSocketsHubLifetimeManager(IServiceConnectionManager<THub> serviceConnectionManager, IHubProtocolResolver protocolResolver) : base(serviceConnectionManager, protocolResolver)
         {
         }
 
         public Task UserAddToGroupAsync(string userId, string groupName, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new ArgumentException(NullOrEmptyStringErrorMessage, nameof(userId));
+            }
+
+            if (string.IsNullOrEmpty(groupName))
+            {
+                throw new ArgumentException(NullOrEmptyStringErrorMessage, nameof(groupName));
+            }
+
+            var message = new UserJoinGroupMessage(userId, groupName);
+            return ServiceConnectionContainer.WriteAsync(message);
         }
 
         public Task UserRemoveFromGroupAsync(string userId, string groupName, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new ArgumentException(NullOrEmptyStringErrorMessage, nameof(userId));
+            }
+
+            if (string.IsNullOrEmpty(groupName))
+            {
+                throw new ArgumentException(NullOrEmptyStringErrorMessage, nameof(groupName));
+            }
+
+            var message = new UserLeaveGroupMessage(userId, groupName);
+            return ServiceConnectionContainer.WriteAsync(message);
         }
     }
 }
