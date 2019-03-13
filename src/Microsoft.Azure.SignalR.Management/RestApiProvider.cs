228791815601f61dfa9f040de8b617533bb36ea6
Wanpeng Li <wanl@microsoft.com>
2019-03-13 15:38:06 +0800

    modify rest api provider

--- src/Microsoft.Azure.SignalR.Management/RestApiProvider.cs
+++ src/Microsoft.Azure.SignalR.Management/RestApiProvider.cs
@@ -10,45 +10,44 @@ namespace Microsoft.Azure.SignalR.Management
         private readonly RestApiAccessTokenGenerator _restApiAccessTokenGenerator;
         private readonly string _baseEndpoint;
         private readonly string _hubName;
-        private readonly string _commonPrefix;
+        private readonly string _commonRequestPrefix;
+        private readonly string _commonAudiencePrefix;
+        private readonly int? _port;
 
         public RestApiProvider(string connectionString, string hubName)
         {
             string accessKey;
-            (_baseEndpoint, accessKey, _, _) = ConnectionStringParser.Parse(connectionString);
+            (_baseEndpoint, accessKey, _, _port) = ConnectionStringParser.Parse(connectionString);
             _hubName = hubName;
             _restApiAccessTokenGenerator = new RestApiAccessTokenGenerator(accessKey);
-            _commonPrefix = $"{_baseEndpoint}/api/v1/hubs/{_hubName}";
+            _commonRequestPrefix = _port == null ? $"{_baseEndpoint}/api/v1/hubs/{_hubName}" : $"{_baseEndpoint}:{_port}/api/v1/hubs/{_hubName}";
+            _commonAudiencePrefix = $"{_baseEndpoint}/api/v1/hubs/{_hubName}";
         }
 
         public RestApiEndpoint GetBroadcastEndpoint(TimeSpan? lifetime = null)
         {
-            var api = _commonPrefix;
-            return GenerateRestApiEndpoint(api, lifetime);
+            return GenerateRestApiEndpoint("", lifetime);
         }
 
         public RestApiEndpoint GetUserGroupManagementEndpoint(string userId, string groupName, TimeSpan? lifetime = null)
         {
-            var api = $"{_commonPrefix}/groups/{groupName}/users/{userId}";
-            return GenerateRestApiEndpoint(api, lifetime);
+            return GenerateRestApiEndpoint($"/groups/{groupName}/users/{userId}", lifetime);
         }
 
         public RestApiEndpoint GetSendToUserEndpoint(string userId, TimeSpan? lifetime = null)
         {
-            var api = $"{_commonPrefix}/users/{userId}";
-            return GenerateRestApiEndpoint(api, lifetime);
+            return GenerateRestApiEndpoint($"/users/{userId}", lifetime);
         }
 
         public RestApiEndpoint GetSendToGroupEndpoint(string groupName, TimeSpan? lifetime = null)
         {
-            var api = $"{_commonPrefix}/groups/{groupName}";
-            return GenerateRestApiEndpoint(api, lifetime);
+            return GenerateRestApiEndpoint($"/groups/{groupName}", lifetime);
         }
 
-        private RestApiEndpoint GenerateRestApiEndpoint(string api, TimeSpan? lifetime = null)
+        private RestApiEndpoint GenerateRestApiEndpoint(string path, TimeSpan? lifetime = null)
         {
-            var token = _restApiAccessTokenGenerator.Generate(api, lifetime);
-            return new RestApiEndpoint(api, token);
+            var token = _restApiAccessTokenGenerator.Generate($"{_commonAudiencePrefix}{path}", lifetime);
+            return new RestApiEndpoint($"{_commonRequestPrefix}{path}", token);
         }
     }
 }
