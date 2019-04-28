550890484eafc34620affb18a4e875440161ac0e
Wanpeng Li <wanl@microsoft.com>
2019-04-28 11:16:07 +0800

    add productInfoAttr

---
+++ src/Microsoft.Azure.SignalR.Common/Utilities/ProductInfoAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Microsoft.Azure.SignalR.Common
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    class ProductInfoAttribute : Attribute
+    {
+        public string PackageName { get; set; }
+        public int Priority { get; set; }
+    }
+}
--- src/Microsoft.Azure.SignalR/ProductInfo.cs
+++ src/Microsoft.Azure.SignalR/ProductInfo.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.Azure.SignalR.Common;
 
 namespace Microsoft.Azure.SignalR
 {
@@ -10,13 +13,35 @@ namespace Microsoft.Azure.SignalR
     {
         public static string GetProductInfo()
         {
-            var assembly = Assembly.GetCallingAssembly();
+            var allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var assemblies = (from assem in allAssemblies
+                              select (Assem: assem, Attr: (ProductInfoAttribute)Attribute.GetCustomAttribute(assem, typeof(ProductInfoAttribute)))
+                              into assemWithAttr
+                              where assemWithAttr.Attr != null
+                              orderby assemWithAttr.Attr.Priority descending
+                              select assemWithAttr).ToList();
+
+            if (assemblies == null || assemblies.Count == 0)
+            {
+                return "";
+            };
+
+            var maxPriority = assemblies.First().Attr.Priority;
+
+            var productInfos = from assem in assemblies
+                               where assem.Attr.Priority == maxPriority
+                               select GetProductInfoCore(assem.Assem);
+
+            return string.Join("; ", productInfos).Substring(0, 256);
+        }
+
+        private static string GetProductInfoCore(Assembly assembly)
+        {
             var packageId = assembly.GetName().Name;
             var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
             var runtime = RuntimeInformation.FrameworkDescription?.Trim();
             var operatingSystem = RuntimeInformation.OSDescription?.Trim();
             var processorArchitecture = RuntimeInformation.ProcessArchitecture.ToString().Trim();
-            
             return $"{packageId}/{version} ({runtime}; {operatingSystem}; {processorArchitecture})";
         }
     }
