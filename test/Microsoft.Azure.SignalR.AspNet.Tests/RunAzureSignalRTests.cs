356e9c4010bd66746645e8cee328e6d9c66a848d
weiyyzz <weiyyzz@qq.com>
2019-05-08 15:55:23 +0800

    Fix test failure

--- test/Microsoft.Azure.SignalR.AspNet.Tests/RunAzureSignalRTests.cs
+++ test/Microsoft.Azure.SignalR.AspNet.Tests/RunAzureSignalRTests.cs
@@ -609,7 +609,7 @@ namespace Microsoft.Azure.SignalR.AspNet.Tests
                     var serverName = token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerName)?.Value;
                     Assert.Equal(name, serverName);
                     var mode = token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerStickyMode)?.Value;
-                    Assert.Equal("Preferred", mode);
+                    Assert.Equal("Required", mode);
                     Assert.NotNull(token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerStickyMode));
                     var version = token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.Version)?.Value;
                     Version.TryParse(version, out var versionResult);
