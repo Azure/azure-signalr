e0d990441e2358e1c8182d3e7977a62147d1b06b
Wanpeng Li <wanl@microsoft.com>
2019-05-10 17:34:45 +0800

    move files

---
+++ test/Microsoft.Azure.SignalR.Tests/UnitTests/NegotiateHandlerFacts.cs
@@ -0,0 +1,389 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Microsoft.Azure.SignalR.Tests
+{
+    public class NegotiateHandlerFacts
+    {
+        private const string CustomClaimType = "custom.claim";
+        private const string CustomUserId = "customUserId";
+        private const string DefaultUserId = "nameId";
+        private const string DefaultConnectionString = "Endpoint=https://localhost;AccessKey=nOu3jXsHnsO5urMumc87M9skQbUWuQ+PE5IvSUEic8w=;";
+        private const string ConnectionString2 = "Endpoint=http://localhost2;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;";
+        private const string ConnectionString3 = "Endpoint=http://localhost3;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;";
+        private const string ConnectionString4 = "Endpoint=http://localhost4;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;";
+        private const string UserPath = "/user/path";
+
+        private static readonly JwtSecurityTokenHandler JwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+        [Theory]
+        [InlineData(typeof(CustomUserIdProvider), CustomUserId)]
+        [InlineData(typeof(NullUserIdProvider), null)]
+        [InlineData(typeof(DefaultUserIdProvider), DefaultUserId)]
+        public void GenerateNegotiateResponseWithUserId(Type type, string expectedUserId)
+        {
+            var config = new ConfigurationBuilder().Build();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(
+                o =>
+                {
+                    o.ConnectionString = DefaultConnectionString;
+                    o.AccessTokenLifetime = TimeSpan.FromDays(1);
+                })
+                .Services
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .AddSingleton(typeof(IUserIdProvider), type)
+                .BuildServiceProvider();
+
+            var httpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(CustomClaimType, CustomUserId),
+                    new Claim(ClaimTypes.NameIdentifier, DefaultUserId),
+                    new Claim("custom", "custom"),
+                }))
+            };
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "hub");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.NotNull(negotiateResponse.Url);
+            Assert.NotNull(negotiateResponse.AccessToken);
+            Assert.Null(negotiateResponse.ConnectionId);
+            Assert.Empty(negotiateResponse.AvailableTransports);
+
+            var token = JwtSecurityTokenHandler.ReadJwtToken(negotiateResponse.AccessToken);
+            Assert.Equal(expectedUserId, token.Claims.FirstOrDefault(x => x.Type == Constants.ClaimType.UserId)?.Value);
+            Assert.Equal("custom", token.Claims.FirstOrDefault(x => x.Type == "custom")?.Value);
+            Assert.Equal(TimeSpan.FromDays(1), token.ValidTo - token.ValidFrom);
+            Assert.Null(token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerName));
+            Assert.Null(token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerStickyMode));
+        }
+
+        [Fact]
+        public void GenerateNegotiateResponseWithUserIdAndServerSticky()
+        {
+            var name = nameof(GenerateNegotiateResponseWithUserIdAndServerSticky);
+            var serverNameProvider = new TestServerNameProvider(name);
+            var config = new ConfigurationBuilder().Build();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(
+                o =>
+                {
+                    o.ServerStickyMode = ServerStickyMode.Required;
+                    o.ConnectionString = DefaultConnectionString;
+                    o.AccessTokenLifetime = TimeSpan.FromDays(1);
+                })
+                .Services
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .AddSingleton(typeof(IUserIdProvider), typeof(DefaultUserIdProvider))
+                .AddSingleton(typeof(IServerNameProvider), serverNameProvider)
+                .BuildServiceProvider();
+
+            var httpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(CustomClaimType, CustomUserId),
+                    new Claim(ClaimTypes.NameIdentifier, DefaultUserId),
+                    new Claim("custom", "custom"),
+                }))
+            };
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "hub");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.NotNull(negotiateResponse.Url);
+            Assert.NotNull(negotiateResponse.AccessToken);
+            Assert.Null(negotiateResponse.ConnectionId);
+            Assert.Empty(negotiateResponse.AvailableTransports);
+
+            var token = JwtSecurityTokenHandler.ReadJwtToken(negotiateResponse.AccessToken);
+            Assert.Equal(DefaultUserId, token.Claims.FirstOrDefault(x => x.Type == Constants.ClaimType.UserId)?.Value);
+            Assert.Equal("custom", token.Claims.FirstOrDefault(x => x.Type == "custom")?.Value);
+            Assert.Equal(TimeSpan.FromDays(1), token.ValidTo - token.ValidFrom);
+
+            var serverName = token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerName)?.Value;
+            Assert.Equal(name, serverName);
+            var mode = token.Claims.FirstOrDefault(s => s.Type == Constants.ClaimType.ServerStickyMode)?.Value;
+            Assert.Equal("Required", mode);
+        }
+
+        [Theory]
+        [InlineData("/user/path/negotiate", "", "asrs.op=%2Fuser%2Fpath")]
+        [InlineData("/user/path/negotiate/", "", "asrs.op=%2Fuser%2Fpath")]
+        [InlineData("", "?customKey=customeValue", "customKey=customeValue")]
+        [InlineData("/user/path/negotiate", "?customKey=customeValue", "asrs.op=%2Fuser%2Fpath&customKey=customeValue")]
+        public void GenerateNegotiateResponseWithPathAndQuery(string path, string queryString, string expectedQueryString)
+        {
+            var config = new ConfigurationBuilder().Build();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(o => o.ConnectionString = DefaultConnectionString)
+                .Services
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .BuildServiceProvider();
+
+            var requestFeature = new HttpRequestFeature
+            {
+                Path = path,
+                QueryString = queryString
+            };
+            var features = new FeatureCollection();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            var httpContext = new DefaultHttpContext(features);
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "chat");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.EndsWith($"?hub=chat&{expectedQueryString}", negotiateResponse.Url);
+        }
+
+        [Theory]
+        [InlineData("", "?hub=chat")]
+        [InlineData("appName", "?hub=appname_chat")]
+        public void GenerateNegotiateResponseWithAppName(string appName, string expectedResponse)
+        {
+            var config = new ConfigurationBuilder().Build();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(o =>
+                {
+                    o.ConnectionString = DefaultConnectionString;
+                    o.ApplicationName = appName;
+                })
+                .Services
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .BuildServiceProvider();
+
+            var requestFeature = new HttpRequestFeature
+            {
+            };
+            var features = new FeatureCollection();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            var httpContext = new DefaultHttpContext(features);
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "chat");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.EndsWith(expectedResponse, negotiateResponse.Url);
+        }
+
+        [Theory]
+        [InlineData(typeof(ConnectionIdUserIdProvider), ServiceHubConnectionContext.ConnectionIdUnavailableError)]
+        [InlineData(typeof(ConnectionAbortedTokenUserIdProvider), ServiceHubConnectionContext.ConnectionAbortedUnavailableError)]
+        [InlineData(typeof(ItemsUserIdProvider), ServiceHubConnectionContext.ItemsUnavailableError)]
+        [InlineData(typeof(ProtocolUserIdProvider), ServiceHubConnectionContext.ProtocolUnavailableError)]
+        public void CustomUserIdProviderAccessUnavailablePropertyThrowsException(Type type, string errorMessage)
+        {
+            var config = new ConfigurationBuilder().Build();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(o => o.ConnectionString = DefaultConnectionString)
+                .Services
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .AddSingleton(typeof(IUserIdProvider), type)
+                .BuildServiceProvider();
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var httpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal()
+            };
+
+            var exception = Assert.Throws<InvalidOperationException>(() => handler.Process(httpContext, "hub"));
+            Assert.Equal(errorMessage, exception.Message);
+        }
+
+        [Fact]
+        public void TestNegotiateHandlerWithMultipleEndpointsAndCustomerRouterAndAppName()
+        {
+            var config = new ConfigurationBuilder().Build();
+            var router = new TestCustomRouter();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(o =>
+                {
+                    o.ApplicationName = "testprefix";
+                    o.Endpoints = new ServiceEndpoint[]
+                    {
+                        new ServiceEndpoint(ConnectionString2),
+                        new ServiceEndpoint(ConnectionString3, name: "chosen"),
+                        new ServiceEndpoint(ConnectionString4),
+                    };
+                })
+                .Services
+                .AddLogging()
+                .AddSingleton<IEndpointRouter>(router)
+                .AddSingleton<IConfiguration>(config)
+                .BuildServiceProvider();
+
+            var requestFeature = new HttpRequestFeature
+            {
+                Path = "/user/path/negotiate/",
+                QueryString = "?endpoint=chosen"
+            };
+
+            var features = new FeatureCollection();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            var httpContext = new DefaultHttpContext(features);
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "chat");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.Equal($"http://localhost3/client/?hub=testprefix_chat&asrs.op=%2Fuser%2Fpath&endpoint=chosen", negotiateResponse.Url);
+        }
+
+        [Fact]
+        public void TestNegotiateHandlerWithMultipleEndpointsAndCustomRouter()
+        {
+            var config = new ConfigurationBuilder().Build();
+            var router = new TestCustomRouter();
+            var serviceProvider = new ServiceCollection().AddSignalR()
+                .AddAzureSignalR(
+                o => o.Endpoints = new ServiceEndpoint[]
+                {
+                    new ServiceEndpoint(ConnectionString2),
+                    new ServiceEndpoint(ConnectionString3, name: "chosen"),
+                    new ServiceEndpoint(ConnectionString4),
+                })
+                .Services
+                .AddLogging()
+                .AddSingleton<IEndpointRouter>(router)
+                .AddSingleton<IConfiguration>(config)
+                .BuildServiceProvider();
+
+            var requestFeature = new HttpRequestFeature
+            {
+                Path = "/user/path/negotiate/",
+                QueryString = "?endpoint=chosen"
+            };
+            var features = new FeatureCollection();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            var httpContext = new DefaultHttpContext(features);
+
+            var handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            var negotiateResponse = handler.Process(httpContext, "chat");
+
+            Assert.NotNull(negotiateResponse);
+            Assert.Equal($"http://localhost3/client/?hub=chat&asrs.op=%2Fuser%2Fpath&endpoint=chosen", negotiateResponse.Url);
+
+            // With no query string should return 400
+            requestFeature = new HttpRequestFeature
+            {
+                Path = "/user/path/negotiate/",
+            };
+
+            var responseFeature = new HttpResponseFeature();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            features.Set<IHttpResponseFeature>(responseFeature);
+            httpContext = new DefaultHttpContext(features);
+
+            handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            negotiateResponse = handler.Process(httpContext, "chat");
+
+            Assert.Null(negotiateResponse);
+
+            Assert.Equal(400, responseFeature.StatusCode);
+
+            // With no query string should return 400
+            requestFeature = new HttpRequestFeature
+            {
+                Path = "/user/path/negotiate/",
+                QueryString = "?endpoint=notexists"
+            };
+
+            responseFeature = new HttpResponseFeature();
+            features.Set<IHttpRequestFeature>(requestFeature);
+            features.Set<IHttpResponseFeature>(responseFeature);
+            httpContext = new DefaultHttpContext(features);
+
+            handler = serviceProvider.GetRequiredService<NegotiateHandler>();
+            Assert.Throws<InvalidOperationException>(() => handler.Process(httpContext, "chat"));
+        }
+
+        private sealed class TestServerNameProvider : IServerNameProvider
+        {
+            private readonly string _serverName;
+            public TestServerNameProvider(string serverName)
+            {
+                _serverName = serverName;
+            }
+
+            public string GetName()
+            {
+                return _serverName;
+            }
+        }
+
+        private class TestCustomRouter : EndpointRouterDecorator
+        {
+            public override ServiceEndpoint GetNegotiateEndpoint(HttpContext context, IEnumerable<ServiceEndpoint> endpoints)
+            {
+                var endpointName = context.Request.Query["endpoint"];
+                if (endpointName.Count == 0)
+                {
+                    context.Response.StatusCode = 400;
+                    var response = Encoding.UTF8.GetBytes("Invalid request");
+                    context.Response.Body.Write(response, 0, response.Length);
+                    return null;
+                }
+
+                return endpoints.First(s => s.Name == endpointName && s.Online);
+            }
+        }
+
+        private class CustomUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection)
+            {
+                return connection.GetHttpContext()?.User?.Claims?.First(c => c.Type == CustomClaimType)?.Value;
+            }
+        }
+
+        private class NullUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection) => null;
+        }
+
+        private class ConnectionIdUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection) => connection.ConnectionId;
+        }
+
+        private class ConnectionAbortedTokenUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection) => connection.ConnectionAborted.IsCancellationRequested.ToString();
+        }
+
+        private class ItemsUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection) => connection.Items.ToString();
+        }
+
+        private class ProtocolUserIdProvider : IUserIdProvider
+        {
+            public string GetUserId(HubConnectionContext connection) => connection.Protocol.Name;
+        }
+    }
+}
---
+++ test/Microsoft.Azure.SignalR.Tests/UnitTests/ServiceEndpointProviderFacts.cs
@@ -0,0 +1,204 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Xunit;
+
+namespace Microsoft.Azure.SignalR.Tests
+{
+    public class ServiceEndpointProviderFacts
+    {
+        private const string Endpoint = "https://myendpoint";
+        private const string AccessKey = "nOu3jXsHnsO5urMumc87M9skQbUWuQ+PE5IvSUEic8w=";
+        private static readonly string HubName = nameof(TestHub).ToLower();
+        private static readonly string AppName = "testapp";
+
+        private static readonly string ConnectionStringWithoutVersion =
+            $"Endpoint={Endpoint};AccessKey={AccessKey};";
+
+        private static readonly string ConnectionStringWithPreviewVersion =
+            $"Endpoint={Endpoint};AccessKey={AccessKey};Version=1.0-preview";
+
+        private static readonly string ConnectionStringWithV1Version = $"Endpoint={Endpoint};AccessKey={AccessKey};Version=1.0";
+
+        private static readonly ServiceEndpointProvider[] EndpointProviderArray =
+        {
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithoutVersion)),
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithPreviewVersion)),
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithV1Version))
+        };
+
+        private static readonly ServiceEndpointProvider[] EndpointProviderArrayWithPrefix =
+{
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithoutVersion), AppName),
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithPreviewVersion), AppName),
+            new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithV1Version), AppName)
+        };
+
+        private static readonly (string path, string queryString, string expectedQuery)[] PathAndQueryArray =
+        {
+            ("", "", ""),
+            (null, "", ""),
+            ("/user/path", "", $"&{Constants.QueryParameter.OriginalPath}=%2Fuser%2Fpath"),
+            ("", "customKey=customValue", "&customKey=customValue"),
+            ("/user/path", "customKey=customValue", $"&{Constants.QueryParameter.OriginalPath}=%2Fuser%2Fpath&customKey=customValue")
+        };
+
+        public static IEnumerable<object[]> DefaultEndpointProviders =>
+            EndpointProviderArray.Select(provider => new object[] {provider});
+
+        public static IEnumerable<object[]> PathAndQueries =>
+            PathAndQueryArray.Select(t => new object[] {t.path, t.queryString, t.expectedQuery});
+
+        public static IEnumerable<object[]> DefaultEndpointProvidersWithPath =>
+            from provider in EndpointProviderArray
+            from t in PathAndQueryArray
+            select new object[] { provider, t.path, t.queryString, t.expectedQuery} ;
+
+        public static IEnumerable<object[]> DefaultEndpointProvidersWithPathPlusPrefix =>
+            from provider in EndpointProviderArrayWithPrefix
+            from t in PathAndQueryArray
+            select new object[] { provider, t.path, t.queryString, t.expectedQuery };
+
+        public static IEnumerable<object[]> DefaultEndpointProvidersPlusPrefix =>
+            EndpointProviderArrayWithPrefix.Select(provider => new object[] { provider });
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProviders))]
+        internal void GetServerEndpoint(IServiceEndpointProvider provider)
+        {
+            var expected = $"{Endpoint}/server/?hub={HubName}";
+            var actual = provider.GetServerEndpoint(nameof(TestHub));
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProvidersWithPath))]
+        internal void GetClientEndpoint(IServiceEndpointProvider provider, string path, string queryString, string expectedQueryString)
+        {
+            var expected = $"{Endpoint}/client/?hub={HubName}{expectedQueryString}";
+            var actual = provider.GetClientEndpoint(HubName, path, queryString);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProvidersPlusPrefix))]
+        internal void GetServerEndpointWithAppName(IServiceEndpointProvider provider)
+        {
+            var expected = $"{Endpoint}/server/?hub={AppName}_{HubName}";
+            var actual = provider.GetServerEndpoint(nameof(TestHub));
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProvidersWithPathPlusPrefix))]
+        internal void GetClientEndpointWithAppName(IServiceEndpointProvider provider, string path, string queryString, string expectedQueryString)
+        {
+            var expected = $"{Endpoint}/client/?hub={AppName}_{HubName}{expectedQueryString}";
+            var actual = provider.GetClientEndpoint(HubName, path, queryString);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        internal void GenerateMutlipleAccessTokenShouldBeUnique()
+        {
+            var count = 1000;
+            var sep = new ServiceEndpointProvider(new ServiceEndpoint(ConnectionStringWithPreviewVersion));
+            var userId = Guid.NewGuid().ToString();
+            var tokens = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                tokens.Add(sep.GenerateClientAccessToken(nameof(TestHub)));
+                tokens.Add(sep.GenerateServerAccessToken(nameof(TestHub), userId));
+            }
+
+            var distinct = tokens.Distinct();
+            Assert.Equal(tokens.Count, distinct.Count());
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProviders))]
+        internal void GenerateServerAccessToken(IServiceEndpointProvider provider)
+        {
+            const string userId = "UserA";
+            var tokenString = provider.GenerateServerAccessToken(nameof(TestHub), userId, requestId: string.Empty);
+            var token = JwtTokenHelper.JwtHandler.ReadJwtToken(tokenString);
+
+            var expectedTokenString = JwtTokenHelper.GenerateJwtBearer($"{Endpoint}/server/?hub={HubName}",
+                new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId)
+                },
+                token.ValidTo,
+                token.ValidFrom,
+                token.ValidFrom,
+                AccessKey,
+                string.Empty);
+
+            Assert.Equal(expectedTokenString, tokenString);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProvidersPlusPrefix))]
+        internal void GenerateServerAccessTokenWithPrefix(IServiceEndpointProvider provider)
+        {
+            const string userId = "UserA";
+            var tokenString = provider.GenerateServerAccessToken(nameof(TestHub), userId, requestId: string.Empty);
+            var token = JwtTokenHelper.JwtHandler.ReadJwtToken(tokenString);
+
+            var expectedTokenString = JwtTokenHelper.GenerateJwtBearer($"{Endpoint}/server/?hub={AppName}_{HubName}",
+                new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId)
+                },
+                token.ValidTo,
+                token.ValidFrom,
+                token.ValidFrom,
+                AccessKey,
+                string.Empty);
+
+            Assert.Equal(expectedTokenString, tokenString);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProviders))]
+        internal void GenerateClientAccessToken(IServiceEndpointProvider provider)
+        {
+            var requestId = Guid.NewGuid().ToString();
+            var tokenString = provider.GenerateClientAccessToken(HubName, requestId: requestId);
+            var token = JwtTokenHelper.JwtHandler.ReadJwtToken(tokenString);
+
+            var expectedTokenString = JwtTokenHelper.GenerateJwtBearer($"{Endpoint}/client/?hub={HubName}",
+                null,
+                token.ValidTo,
+                token.ValidFrom,
+                token.ValidFrom,
+                AccessKey,
+                requestId);
+
+            Assert.Equal(expectedTokenString, tokenString);
+        }
+
+        [Theory]
+        [MemberData(nameof(DefaultEndpointProvidersPlusPrefix))]
+        internal void GenerateClientAccessTokenWithPrefix(IServiceEndpointProvider provider)
+        {
+            var requestId = Guid.NewGuid().ToString();
+            var tokenString = provider.GenerateClientAccessToken(HubName, requestId: requestId);
+            var token = JwtTokenHelper.JwtHandler.ReadJwtToken(tokenString);
+
+            var expectedTokenString = JwtTokenHelper.GenerateJwtBearer($"{Endpoint}/client/?hub={AppName}_{HubName}",
+                null,
+                token.ValidTo,
+                token.ValidFrom,
+                token.ValidFrom,
+                AccessKey,
+                requestId);
+
+            Assert.Equal(expectedTokenString, tokenString);
+        }
+    }
+}
