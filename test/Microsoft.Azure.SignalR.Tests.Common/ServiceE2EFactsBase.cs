8f1daf9e6931f4ce2cf123fe1cd9ee7005d0c04f
Wanpeng Li <wanl@microsoft.com>
2019-05-21 11:35:12 +0800

    update

--- test/Microsoft.Azure.SignalR.Tests.Common/ServiceE2EFactsBase.cs
+++ test/Microsoft.Azure.SignalR.Tests.Common/ServiceE2EFactsBase.cs
@@ -5,7 +5,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Testing.xunit;
-using Microsoft.Extensions.Logging;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,27 +12,27 @@ namespace Microsoft.Azure.SignalR.Tests.Common
 {
     public class ServiceE2EFactsBase : VerifiableLoggedTest, IDisposable
     {
-        protected const int DefaultClientCount = 3;
-        protected const int DefaultDelayMs = 2000;
-        protected string _defaultMessage = $"Message from {nameof(ServiceE2EFactsBase)}";
-        protected string _serverUrl;
-        protected ITestServer _testServer;
-        protected Func<string, int, ITestClientSet> _testClientSetFactory;
-        protected ILoggerFactory _loggerFactory;
+        private const int DefaultClientCount = 3;
+        private const int DefaultDelayMilliseconds = 2000;
+        private static readonly string _defaultMessage = $"Message from {nameof(ServiceE2EFactsBase)}";
+        private string _serverUrl;
+        private ITestServer _testServer;
+        private Func<string, int, ITestClientSet> _testClientSetFactory;
+        private IDisposable _verifiableLog;
 
         public ServiceE2EFactsBase(ITestServer testServer, Func<string, int, ITestClientSet> testClientSetFactory, ITestOutputHelper output) : base(output)
         {
             _testServer = testServer;
             _testClientSetFactory = testClientSetFactory;
 
-            StartVerifiableLog(out _loggerFactory);
-            _serverUrl = _testServer.StartAsync(_loggerFactory).Result;
+            _verifiableLog = StartVerifiableLog(out var loggerFactory);
+            _serverUrl = _testServer.StartAsync(loggerFactory).Result;
         }
 
-        public async void Dispose()
+        public void Dispose()
         {
-            await _testServer.StopAsync();
-            _loggerFactory.Dispose();
+            _testServer.StopAsync().Wait();
+            _verifiableLog.Dispose();
         }
 
         [ConditionalFact]
@@ -46,7 +45,7 @@ namespace Microsoft.Azure.SignalR.Tests.Common
             clients.AddListener(methodName, message => Interlocked.Increment(ref count));
             await clients.StartAsync();
             await clients.AllSendAsync(methodName, _defaultMessage);
-            await Task.Delay(DefaultDelayMs);
+            await Task.Delay(DefaultDelayMilliseconds);
             await clients.StopAsync();
             Assert.Equal(clients.Count, count);
         }
