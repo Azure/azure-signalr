082eaa2a67c94a72b99fac2cc3fe241c8bf0c444
Wanpeng Li <wanl@microsoft.com>
2019-05-22 12:21:27 +0800

    e2e test for asp.net/asp.net core: echo (#533)

---
+++ test/Microsoft.Azure.SignalR.AspNet.Tests/E2ETest/TestServer.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.SignalR.Tests.Common;
+using Microsoft.Extensions.Logging;
+using Microsoft.Owin.Hosting;
+
+namespace Microsoft.Azure.SignalR.AspNet.Tests
+{
+    internal class TestServer : TestServerBase
+    {
+        private IDisposable _webApp;
+
+        protected override Task StartCoreAsync(string serverUrl, ILoggerFactory loggerFactory)
+        {
+            var startOpts = new StartOptions(serverUrl);
+            _webApp = WebApp.Start<TestStartup>(startOpts);
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync()
+        {
+            _webApp.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ test/Microsoft.Azure.SignalR.Tests/E2ETest/TestServer.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.SignalR.Tests.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.SignalR.Tests
+{
+    internal class TestServer : TestServerBase
+    {
+        private IWebHost _host;
+
+        protected override Task StartCoreAsync(string serverUrl, ILoggerFactory loggerFactory)
+        {
+            _host = new WebHostBuilder()
+                .UseStartup<TestStartup>()
+                .UseUrls(serverUrl)
+                .UseKestrel()
+                .ConfigureLogging((ILoggingBuilder logging) =>
+                {
+                    logging.Services.AddSingleton(typeof(ILoggerFactory), loggerFactory);
+                    logging.Services.AddLogging();
+                })
+                .Build();
+            return _host.StartAsync();
+        }
+
+        public override async Task StopAsync()
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+        }
+    }
+}
