cfc2cffbe97bc90300902b0d7346b5b0a732e715
wanl <wanl@microsoft.com>
2019-10-31 17:34:42 +0800

    add appsetings

--- test/Microsoft.Azure.SignalR.Tests.Common/TestConfiguration.cs
+++ test/Microsoft.Azure.SignalR.Tests.Common/TestConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
 
@@ -17,8 +18,9 @@ namespace Microsoft.Azure.SignalR.Tests.Common
         protected TestConfiguration()
         {
             _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
                 .AddUserSecrets<TestConfiguration>(optional: true)
-                .AddJsonFile("../appsettings.Test.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Test.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
 
