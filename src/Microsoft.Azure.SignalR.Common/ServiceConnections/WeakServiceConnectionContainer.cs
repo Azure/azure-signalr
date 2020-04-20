180c2951678f56fc988a33469597f1a4424855b5
Liangying.Wei <lianwei@microsoft.com>
2020-04-20 11:13:31 +0800

    Move active to ServiceEndpoint so that user can decide whether send or not (#879)
    * Move active to ServiceEndpoint so that user can decide whether to send messages or not instead of making it a default behavior of weak connections
    
    * resolve comments

--- src/Microsoft.Azure.SignalR.Common/ServiceConnections/WeakServiceConnectionContainer.cs
+++ src/Microsoft.Azure.SignalR.Common/ServiceConnections/WeakServiceConnectionContainer.cs
@@ -3,23 +3,12 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.Azure.SignalR.Protocol;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.SignalR.Common
 {
     internal class WeakServiceConnectionContainer : ServiceConnectionContainerBase
     {
-        private const int CheckWindow = 5;
-        private static readonly TimeSpan CheckTimeSpan = TimeSpan.FromMinutes(10);
-
-        private readonly object _lock = new object();
-        private int _inactiveCount;
-        private DateTime? _firstInactiveTime;
-
-        // active ones are those whose client connections connected to the whole endpoint
-        private volatile bool _active = true;
-
         protected override ServiceConnectionType InitialConnectionType => ServiceConnectionType.Weak;
 
         public WeakServiceConnectionContainer(IServiceConnectionFactory serviceConnectionFactory,
@@ -28,62 +17,9 @@ namespace Microsoft.Azure.SignalR.Common
         {
         }
 
-        public override Task HandlePingAsync(PingMessage pingMessage)
-        {
-            base.HandlePingAsync(pingMessage);
-            var active = HasClients;
-            _active = GetServiceStatus(active, CheckWindow, CheckTimeSpan);
-
-            return Task.CompletedTask;
-        }
-
-        public override Task WriteAsync(ServiceMessage serviceMessage)
-        {
-            if (!_active && !(serviceMessage is PingMessage))
-            {
-                // If the endpoint is inactive, there is no need to send messages to it
-                Log.IgnoreSendingMessageToInactiveEndpoint(Logger, serviceMessage.GetType(), Endpoint);
-                return Task.CompletedTask;
-            }
-
-            return base.WriteAsync(serviceMessage);
-        }
-
         public override Task OfflineAsync(bool migratable)
         {
             return Task.CompletedTask;
-        }
-
-        internal bool GetServiceStatus(bool active, int checkWindow, TimeSpan checkTimeSpan)
-        {
-            lock (_lock)
-            {
-                if (active)
-                {
-                    _firstInactiveTime = null;
-                    _inactiveCount = 0;
-                    return true;
-                }
-                else
-                {
-                    if (_firstInactiveTime == null)
-                    {
-                        _firstInactiveTime = DateTime.UtcNow;
-                    }
-
-                    _inactiveCount++;
-
-                    // Inactive it only when it checks over 5 times and elapsed for over 10 minutes
-                    if (_inactiveCount >= checkWindow && DateTime.UtcNow - _firstInactiveTime >= checkTimeSpan)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-            }
         }
 
         private static class Log
