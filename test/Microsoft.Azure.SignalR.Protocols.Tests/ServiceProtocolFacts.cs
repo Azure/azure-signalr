ac510cb622502034404e9af86e66926cd2d05f61
Zhenghui Yan <zhyan@microsoft.com>
2020-04-29 14:32:16 +0800

    add more compatibility data.

--- test/Microsoft.Azure.SignalR.Protocols.Tests/ServiceProtocolFacts.cs
+++ test/Microsoft.Azure.SignalR.Protocols.Tests/ServiceProtocolFacts.cs
@@ -73,6 +73,165 @@ namespace Microsoft.Azure.SignalR.Protocol.Tests
                 name: "HandshakeRequestWithProperty",
                 message: new HandshakeRequestMessage(1) { ConnectionType = 1, Target = "abc" },
                 binary: "lAEBAaNhYmM="),
+            new ProtocolTestData(
+                name: "HandshakeRequest_NoOptionalField",
+                message: new HandshakeRequestMessage(1),
+                binary: "lQEBAKAA"),
+            new ProtocolTestData(
+                name: "HandshakeRequestWithProperty_NoOptionalField",
+                message: new HandshakeRequestMessage(1) { ConnectionType = 1, Target = "abc" },
+                binary: "lQEBAaNhYmMA"),
+            new ProtocolTestData(
+                name: "HandshakeRequestWithMigratableStatus_NoOptionalField",
+                message: new HandshakeRequestMessage(1) { MigrationLevel = 1},
+                binary: "lQEBAKAB"),
+            new ProtocolTestData(
+                name: "HandshakeResponse_NoOptionalField",
+                message: new HandshakeResponseMessage(),
+                binary: "kgKg"),
+            new ProtocolTestData(
+                name: "HandshakeResponseWithError_NoOptionalField",
+                message: new HandshakeResponseMessage("Version mismatch."),
+                binary: "kgKxVmVyc2lvbiBtaXNtYXRjaC4="),
+            new ProtocolTestData(
+                name: "OpenConnection_NoOptionalField",
+                message: new OpenConnectionMessage("conn1", null),
+                binary: "lQSlY29ubjGAgKA="),
+            new ProtocolTestData(
+                name: "OpenConnectionWithClaims_NoOptionalField",
+                message: new OpenConnectionMessage("conn2", new [] {new Claim(ClaimTypes.NameIdentifier, "user1")}),
+                binary: "lQSlY29ubjKB2URodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllcqV1c2VyMYCg"),
+            new ProtocolTestData(
+                name: "OpenConnectionWithHeaders_NoOptionalField",
+                message: new OpenConnectionMessage("conn3", null, new Dictionary<string, StringValues>
+                {
+                    {"header-key-1", "header-value-1"},
+                    {"header-key-2", new[] {"heaer-value-2a", "header-value-2b"}},
+                    {"header-key-3", new[] {"heaer-value-3a", "header-value-3b", "header-value-3c"}}
+                }, string.Empty),
+                binary: "lQSlY29ubjOAg6xoZWFkZXIta2V5LTGRrmhlYWRlci12YWx1ZS0xrGhlYWRlci1rZXktMpKuaGVhZXItdmFsdWUtMmGvaGVhZGVyLXZhbHVlLTJirGhlYWRlci1rZXktM5OuaGVhZXItdmFsdWUtM2GvaGVhZGVyLXZhbHVlLTNir2hlYWRlci12YWx1ZS0zY6A="),
+            new ProtocolTestData(
+                name: "OpenConnectionWithQueryString1_NoOptionalField",
+                message: new OpenConnectionMessage("conn4", null, new Dictionary<string, StringValues>(), "query1=value1"),
+                binary: "lQSlY29ubjSAgK1xdWVyeTE9dmFsdWUx"),
+            new ProtocolTestData(
+                name: "OpenConnectionWithQueryString2_NoOptionalField",
+                message: new OpenConnectionMessage("conn4", null, new Dictionary<string, StringValues>(), "query1=value1&query2=query2&query3=value3"),
+                binary: "lQSlY29ubjSAgNkpcXVlcnkxPXZhbHVlMSZxdWVyeTI9cXVlcnkyJnF1ZXJ5Mz12YWx1ZTM="),
+            new ProtocolTestData(
+                name: "CloseConnection_NoOptionalField",
+                message: new CloseConnectionMessage("conn3"),
+                binary: "lAWlY29ubjOggA=="),
+            new ProtocolTestData(
+                name: "CloseConnectionWithError_NoOptionalField",
+                message: new CloseConnectionMessage("conn4", "Error message."),
+                binary: "lAWlY29ubjSuRXJyb3IgbWVzc2FnZS6A"),
+            new ProtocolTestData(
+                name: "ConnectionData_NoOptionalField",
+                message: new ConnectionDataMessage("conn5", new byte[] {1, 2, 3, 4, 5, 6, 7}),
+                binary: "kwalY29ubjXEBwECAwQFBgc="),
+            new ProtocolTestData(
+                name: "MultiConnectionData_NoOptionalField",
+                message: new MultiConnectionDataMessage(new [] {"conn6", "conn7"}, new Dictionary<string, ReadOnlyMemory<byte>>
+                {
+                    ["json"] = new byte[] {2, 3, 4, 5, 6, 7, 1},
+                    ["messagepack"] = new byte[] {3, 4, 5, 6, 7, 1, 2}
+                }),
+                binary: "kweSpWNvbm42pWNvbm43gqRqc29uxAcCAwQFBgcBq21lc3NhZ2VwYWNrxAcDBAUGBwEC"),
+            new ProtocolTestData(
+                name: "UserData_NoOptionalField",
+                message: new UserDataMessage("user1",
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {6, 7, 1, 2, 3, 4, 5},
+                        ["messagepack"] = new byte[] {7, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "kwildXNlcjGCpGpzb27EBwYHAQIDBAWrbWVzc2FnZXBhY2vEBwcBAgMEBQY="),
+            new ProtocolTestData(
+                name: "MultiUserData_NoOptionalField",
+                message: new MultiUserDataMessage(new [] {"user1", "user2"},
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {6, 7, 1, 2, 3, 4, 5},
+                        ["messagepack"] = new byte[] {7, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "kwmSpXVzZXIxpXVzZXIygqRqc29uxAcGBwECAwQFq21lc3NhZ2VwYWNrxAcHAQIDBAUG"),
+            new ProtocolTestData(
+                name: "Broadcast_NoOptionalField",
+                message: new BroadcastDataMessage(new Dictionary<string, ReadOnlyMemory<byte>>
+                {
+                    ["json"] = new byte[] {4, 5, 6, 7, 1, 2, 3},
+                    ["messagepack"] = new byte[] {5, 6, 7, 1, 2, 3, 4}
+                }),
+                binary: "kwqQgqRqc29uxAcEBQYHAQIDq21lc3NhZ2VwYWNrxAcFBgcBAgME"),
+            new ProtocolTestData(
+                name: "BroadcastExcept_NoOptionalField",
+                message: new BroadcastDataMessage(new[] {"conn7", "conn8", "conn9"},
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {6, 7, 1, 2, 3, 4, 5},
+                        ["messagepack"] = new byte[] {7, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "kwqTpWNvbm43pWNvbm44pWNvbm45gqRqc29uxAcGBwECAwQFq21lc3NhZ2VwYWNrxAcHAQIDBAUG"),
+            new ProtocolTestData(
+                name: "JoinGroup_NoOptionalField",
+                message: new JoinGroupMessage("conn10", "group1"),
+                binary: "kwumY29ubjEwpmdyb3VwMQ=="),
+            new ProtocolTestData(
+                name: "LeaveGroup_NoOptionalField",
+                message: new LeaveGroupMessage("conn11", "group2"),
+                binary: "kwymY29ubjExpmdyb3VwMg=="),
+            new ProtocolTestData(
+                name: "UserJoinGroup_NoOptionalField",
+                message: new UserJoinGroupMessage("conn10", "group1"),
+                binary: "kxCmY29ubjEwpmdyb3VwMQ=="),
+            new ProtocolTestData(
+                name: "UserLeaveGroup_NoOptionalField",
+                message: new UserLeaveGroupMessage("conn11", "group2"),
+                binary: "kxGmY29ubjExpmdyb3VwMg=="),
+            new ProtocolTestData(
+                name: "GroupBroadcast_NoOptionalField",
+                message: new GroupBroadcastDataMessage("group3",
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {6, 7, 1, 2, 3, 4, 5},
+                        ["messagepack"] = new byte[] {7, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "lA2mZ3JvdXAzkIKkanNvbsQHBgcBAgMEBattZXNzYWdlcGFja8QHBwECAwQFBg=="),
+            new ProtocolTestData(
+                name: "GroupBroadcastExcept_NoOptionalField",
+                message: new GroupBroadcastDataMessage("group3", new [] {"conn12", "conn13"},
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {6, 7, 1, 2, 3, 4, 5},
+                        ["messagepack"] = new byte[] {7, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "lA2mZ3JvdXAzkqZjb25uMTKmY29ubjEzgqRqc29uxAcGBwECAwQFq21lc3NhZ2VwYWNrxAcHAQIDBAUG"),
+            new ProtocolTestData(
+                name: "MultiGroupBroadcast_NoOptionalField",
+                message: new MultiGroupBroadcastDataMessage(new [] {"group4", "group5"},
+                    new Dictionary<string, ReadOnlyMemory<byte>>
+                    {
+                        ["json"] = new byte[] {1, 2, 3, 4, 5, 6, 7, 8},
+                        ["messagepack"] = new byte[] {7, 8, 1, 2, 3, 4, 5, 6}
+                    }),
+                binary: "kw6Spmdyb3VwNKZncm91cDWCpGpzb27ECAECAwQFBgcIq21lc3NhZ2VwYWNrxAgHCAECAwQFBg=="),
+            new ProtocolTestData(
+                name: "JoinGroupWithAck_NoOptionalField",
+                message: new JoinGroupWithAckMessage("conn14", "group1", 1),
+                binary: "lBKmY29ubjE0pmdyb3VwMQE="),
+            new ProtocolTestData(
+                name: "LeaveGroupWithAck_NoOptionalField",
+                message: new LeaveGroupWithAckMessage("conn15", "group2", 1),
+                binary: "lBOmY29ubjE1pmdyb3VwMgE="),
+            new ProtocolTestData(
+                name: "Ack_NoOptionalField",
+                message: new AckMessage(1, 100),
+                binary: "lBQBZKA="),
+            new ProtocolTestData(
+                name: "AckWithMessage_NoOptionalField",
+                message: new AckMessage(2, 101, "Joined group successfully"),
+                binary: "lBQCZblKb2luZWQgZ3JvdXAgc3VjY2Vzc2Z1bGx5"),
         }.ToDictionary(t => t.Name);
 
         public static IDictionary<string, ProtocolTestData> TestData => new[]
@@ -238,15 +397,15 @@ namespace Microsoft.Azure.SignalR.Protocol.Tests
                 binary: "kg/ZK01heGltdW0gbWVzc2FnZSBjb3VudCBsaW1pdCByZWFjaGVkOiAxMDAwMDA="),
             new ProtocolTestData(
                 name: "JoinGroupWithAck",
-                message: new JoinGroupWithAckMessage("conn14", "group1", 1), 
+                message: new JoinGroupWithAckMessage("conn14", "group1", 1),
                 binary: "lRKmY29ubjE0pmdyb3VwMQGA"),
             new ProtocolTestData(
                 name: "LeaveGroupWithAck",
-                message: new LeaveGroupWithAckMessage("conn15", "group2", 1), 
+                message: new LeaveGroupWithAckMessage("conn15", "group2", 1),
                 binary: "lROmY29ubjE1pmdyb3VwMgGA"),
             new ProtocolTestData(
                 name: "Ack",
-                message: new AckMessage(1, 100), 
+                message: new AckMessage(1, 100),
                 binary: "lRQBZKCA"),
             new ProtocolTestData(
                 name: "AckWithMessage",
@@ -360,17 +519,17 @@ Please verify the MsgPack output and update the baseline");
 
         private static byte ArrayBytes(int size)
         {
-            return (byte) (0x90 | size);
+            return (byte)(0x90 | size);
         }
 
         private static byte StringBytes(int size)
         {
-            return (byte) (0xa0 | size);
+            return (byte)(0xa0 | size);
         }
 
         private static byte MapBytes(int size)
         {
-            return (byte) (0x80 | size);
+            return (byte)(0x80 | size);
         }
 
         private static byte[] Frame(byte[] input)
