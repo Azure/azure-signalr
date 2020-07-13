09896a2bac8bc83b6b710a87106bdd9e08d9bb7b
wanl <wanl@microsoft.com>
2020-07-13 11:43:30 +0800

    generate message tracing id

---
+++ src/Microsoft.Azure.SignalR.Common/ServiceMessages/IMessageWithTracingIdExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.SignalR.Protocol;
+
+namespace Microsoft.Azure.SignalR
+{
+    internal static class IMessageWithTracingIdExtensions
+    {
+        public static ExtensibleServiceMessage WithTracingId<T>(this T message) where T : ExtensibleServiceMessage, IMessageWithTracingId
+        {
+            if (ServiceConnectionContainerScope.EnableMessageLog || ClientConnectionScope.IsDiagnosticClient)
+            {
+                var id = MessageWithTracingIdHelper.Generate(ClientConnectionScope.IsDiagnosticClient);
+                message.TracingId = id;
+                return message;
+            }
+            return message;
+        }
+    }
+}
---
+++ src/Microsoft.Azure.SignalR.Common/ServiceMessages/MessageWithTracingIdHelper.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+
+namespace Microsoft.Azure.SignalR
+{
+    internal static class MessageWithTracingIdHelper
+    {
+        internal static ulong Prefix { get; set; } = (ulong)Guid.NewGuid().GetHashCode();
+        private static long _index = 0;
+
+        // message tracing id is constructed in the format:
+        // from most significant digit to least significant digit:
+        // 1 hex digits: isFromTracingClient (unused bit are preserved),
+        // 7 hex digits: prefix of the server
+        // 8 hex digits: message index
+        public static ulong Generate(bool isFromTracingClient)
+        {
+            ulong prefixWithPreserve = Prefix & 0x0FFF_FFFF;
+            ulong tracingClientMask = (ulong)(isFromTracingClient ? 0x1000_0000 : 0);
+            var id = ((prefixWithPreserve | tracingClientMask) << 32) + (ulong)_index;
+            Interlocked.Increment(ref _index);
+            return id;
+        }
+    }
+}
