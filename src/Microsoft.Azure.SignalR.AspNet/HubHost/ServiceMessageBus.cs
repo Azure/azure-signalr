51056ba752cb8bfd4c1b6a6f82cf2e8af8f995e4
wanl <wanl@microsoft.com>
2020-09-07 11:24:16 +0800

    add msglog

--- src/Microsoft.Azure.SignalR.AspNet/HubHost/ServiceMessageBus.cs
+++ src/Microsoft.Azure.SignalR.AspNet/HubHost/ServiceMessageBus.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Infrastructure;
 using Microsoft.AspNet.SignalR.Messaging;
 using Microsoft.Azure.SignalR.Protocol;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.SignalR.AspNet
 {
@@ -17,14 +18,16 @@ namespace Microsoft.Azure.SignalR.AspNet
         private readonly IServiceConnectionManager _serviceConnectionManager;
         private readonly IClientConnectionManager _clientConnectionManager;
         private readonly IAckHandler _ackHandler;
+        private readonly ILogger<ServiceMessageBus> _logger;
 
-        public ServiceMessageBus(IDependencyResolver resolver) : base(resolver)
+        public ServiceMessageBus(IDependencyResolver resolver, ILogger<ServiceMessageBus> logger) : base(resolver)
         {
             // TODO: find a more decent way instead of DI, it can be easily overriden
             _serviceConnectionManager = resolver.Resolve<IServiceConnectionManager>() ?? throw new ArgumentNullException(nameof(IServiceConnectionManager));
             _clientConnectionManager = resolver.Resolve<IClientConnectionManager>() ?? throw new ArgumentNullException(nameof(IClientConnectionManager));
             _parser = resolver.Resolve<IMessageParser>() ?? throw new ArgumentNullException(nameof(IMessageParser));
             _ackHandler = resolver.Resolve<IAckHandler>() ?? throw new ArgumentNullException(nameof(IAckHandler));
+            _logger = logger ?? throw new ArgumentNullException(nameof(ILogger<ServiceMessageBus>));
         }
 
         public override Task Publish(Message message)
@@ -43,14 +46,31 @@ namespace Microsoft.Azure.SignalR.AspNet
             return Task.WhenAll(messages.Select(m => ProcessMessage(m)));
         }
 
-        private Task ProcessMessage(AppMessage message)
+        private async Task ProcessMessage(AppMessage message)
         {
             if (message is HubMessage hubMessage)
             {
-                return WriteMessage(_serviceConnectionManager.WithHub(hubMessage.HubName), message);
+                try
+                {
+                    await WriteMessage(_serviceConnectionManager.WithHub(hubMessage.HubName), message);
+                    if (hubMessage.Message is IMessageWithTracingId msg && msg.TracingId != null)
+                    {
+                        AzureSignalRLog.SucceededToSendMessage(_logger, msg);
+                    }
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    if (hubMessage.Message is IMessageWithTracingId msg && msg.TracingId != null)
+                    {
+                        AzureSignalRLog.FailedToSendMessage(_logger, msg, ex);
+                    }
+                    throw;
+                }
             }
 
-            return WriteMessage(_serviceConnectionManager, message);
+            await WriteMessage(_serviceConnectionManager, message);
+            return;
         }
 
         private async Task WriteMessage(IServiceConnectionContainer connection, AppMessage appMessage)
