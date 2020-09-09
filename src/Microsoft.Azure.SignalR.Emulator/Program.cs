b055d2f03586576ad3730b70e76c2efd95427726
Liangying Wei <lianwei@microsoft.com>
2020-09-09 14:04:31 +0800

    minor fix

--- src/Microsoft.Azure.SignalR.Emulator/Program.cs
+++ src/Microsoft.Azure.SignalR.Emulator/Program.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.IO;
-using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Configuration;
