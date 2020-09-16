f08831465cfa134316937920df2d2e0ff7826747
yzt <Y-Sindo@users.noreply.github.com>
2020-09-16 14:39:25 +0800

    Replace old REST clients with generated one (#1036)
    * Replace old REST clients with generated one

--- test/Microsoft.Azure.SignalR.Common.Tests/RestClients/RestClientBuilderFacts.cs
+++ test/Microsoft.Azure.SignalR.Common.Tests/RestClients/RestClientBuilderFacts.cs
@@ -13,22 +13,19 @@ namespace Microsoft.Azure.SignalR.Common.Tests.RestClients
     {
         private const string AccessKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         private const string Endpoint = "http://endpoint/";
+        private const string productInfo = "productInfo";
         private readonly string _connectionString = $"Endpoint={Endpoint};AccessKey={AccessKey};Version=1.0;";
 
         [Fact]
         public void RequestContainsAsrsUAFact()
         {
-            string productInfo = "productInfo";
-
-            void action(RestClientBuilder b) => b.WithUserAgent(productInfo);
-
             void assertion(HttpRequestMessage request, CancellationToken t)
             {
                 Assert.True(request.Headers.Contains(Constants.AsrsUserAgent));
                 Assert.NotNull(request.Headers.GetValues(Constants.AsrsUserAgent));
             }
 
-            TestRestClientBuilder(assertion, action);
+            TestRestClientBuilder(assertion);
         }
 
         [Fact]
@@ -43,25 +40,24 @@ namespace Microsoft.Azure.SignalR.Common.Tests.RestClients
                 Assert.Equal("Bearer", scheme);
                 Assert.NotNull(parameter);
             }
-            TestRestClientBuilder(assertion, null);
+            TestRestClientBuilder(assertion);
         }
 
         [Fact]
         public void GetCustomiazeClient_BaseUriRightFact()
         {
-            RestClientBuilder restClientBuilder = new RestClientBuilder(_connectionString);
+            RestClientBuilder restClientBuilder = new RestClientBuilder(_connectionString, productInfo);
             using var restClient = restClientBuilder.Build();
 
             Assert.Equal(Endpoint, restClient.BaseUri.AbsoluteUri);
         }
 
-        private async void TestRestClientBuilder(Action<HttpRequestMessage, CancellationToken> assertion, Action<RestClientBuilder> action)
+        private async void TestRestClientBuilder(Action<HttpRequestMessage, CancellationToken> assertion)
         {
             var handler = new TestRootHandler(assertion);
-            RestClientBuilder restClientBuilder = new RestClientBuilder(_connectionString)
+            RestClientBuilder restClientBuilder = new RestClientBuilder(_connectionString, productInfo)
                 .WithRootHandler(handler);
 
-            action?.Invoke(restClientBuilder);
             using var restClient = restClientBuilder.Build();
             await restClient.HealthApi.GetHealthStatusWithHttpMessagesAsync();
         }
