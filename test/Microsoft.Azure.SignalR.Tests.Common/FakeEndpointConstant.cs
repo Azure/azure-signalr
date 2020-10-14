6fc6d6744d5f23f354524c9d79524037caf3d8f1
Zitong Yang <zityang@microsoft.com>
2020-10-14 11:46:10 +0800

    add unit test for create hub context method

---
+++ test/Microsoft.Azure.SignalR.Tests.Common/FakeEndpointConstant.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+
+namespace Microsoft.Azure.SignalR.Tests.Common
+{
+    public static class FakeEndpointConstant
+    {
+        public const string FakeAccessKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGH";
+        public static readonly string[] FakeEndpointUrls = new string[] { "http://a", "http://b", "http://c" };
+        public static readonly string[] FakeConnectionStrings = FakeEndpointUrls.Select(url => $"Endpoint={url};AccessKey={FakeAccessKey};Version=1.0;").ToArray();
+        public static readonly ServiceEndpoint[] FakeServiceEndpoints = FakeConnectionStrings.Select(connectionString => new ServiceEndpoint(connectionString)).ToArray();
+    }
+}
