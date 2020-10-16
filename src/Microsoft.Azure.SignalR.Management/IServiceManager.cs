a314b4190614357d32328f3eb9c1c072522279ce
Zitong Yang <zityang@microsoft.com>
2020-10-16 11:23:15 +0800

    commit

--- src/Microsoft.Azure.SignalR.Management/IServiceManager.cs
+++ src/Microsoft.Azure.SignalR.Management/IServiceManager.cs
@@ -25,8 +25,6 @@ namespace Microsoft.Azure.SignalR.Management
         /// <returns>The task object representing the asynchronous operation.</returns>
         Task<IServiceHubContext> CreateHubContextAsync(string hubName, ILoggerFactory loggerFactory = null, CancellationToken cancellationToken = default);
 
-
-
         /// <summary>
         /// Creates a client access token for SignalR hub connections to connect to Azure SignalR Service.
         /// </summary>
@@ -35,6 +33,7 @@ namespace Microsoft.Azure.SignalR.Management
         /// <param name="claims">The claim list to be put into access token.</param>
         /// <param name="lifeTime">The lifetime of the token. The default value is one hour.</param>
         /// <returns>Client access token to Azure SignalR Service.</returns>
+        [Obsolete("Please get an endpoint via method GetClientEndpoint(HttpContext) and then use the extension method of ServiceEndpoint.")]
         string GenerateClientAccessToken(string hubName, string userId = null, IList<Claim> claims = null, TimeSpan? lifeTime = null);
 
         /// <summary>
@@ -42,36 +41,15 @@ namespace Microsoft.Azure.SignalR.Management
         /// </summary>
         /// <param name="hubName">The hub name.</param>
         /// <returns>Client endpoint URL to Azure SignalR Service.</returns>
+        [Obsolete("Please get an endpoint via method GetClientEndpoint(HttpContext) and then use the extension method of ServiceEndpoint.")]
         string GetClientEndpoint(string hubName);
-
 
         /// <summary>
         /// Selects an endpoint randomly with <see cref="IEndpointRouter"/>
         /// </summary>
         /// <param name="httpContext"></param>
         /// <returns>Client endpoint object to Azure SignalR Service.</returns>
-        ServiceEndpoint GetClientEndpoint(HttpContext httpContext);
-
-        /// <summary>
-        /// Gets the URL of a specified client endpoint for SignalR hub connections to connect to Azure SignalR Service.
-        /// </summary>
-        /// <param name="hubName">The hub name.</param>
-        /// <param name="endpoint">The endpoint object.</param>
-        /// <returns>Client endpoint URL to Azure SingalR Service</returns>
-        string GetClientEndpoint(string hubName, ServiceEndpoint endpoint);
-
-        /// <summary>
-        /// Creates a client access token for SignalR hub connections to connect to Azure SignalR Service.
-        /// </summary>
-        /// <param name="hubName">The hub name.</param>
-        /// <param name="endpoint">The client endpoint.</param>
-        /// <param name="userId">The user ID.</param>
-        /// <param name="claims">The claim list to be put into access token.</param>
-        /// <param name="lifeTime">The lifetime of the token. The default value is one hour.</param>
-        /// <returns>Client access token to Azure SignalR Service.</returns>
-        string GenerateClientAccessToken(string hubName, ServiceEndpoint endpoint, string userId = null, IList<Claim> claims = null, TimeSpan? lifeTime = null);
-
-
+        ServiceEndpoint GetClientEndpoint(HttpContext httpContext = null);
 
         /// <summary>
         /// Checks the health status of the Azure SignalR Service.
