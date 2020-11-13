bedd51e36299840f4c76f4ad1db8ab1a7624d416
Zitong Yang <zityang@microsoft.com>
2020-11-13 19:15:26 +0800

    Add parse multiendpoint test

--- test/Microsoft.Azure.SignalR.Tests/ServiceOptionsSetupFacts.cs
+++ test/Microsoft.Azure.SignalR.Tests/ServiceOptionsSetupFacts.cs
@@ -3,6 +3,8 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
@@ -10,7 +12,7 @@ namespace Microsoft.Azure.SignalR.Tests
 {
     public class ServiceOptionsSetupFacts
     {
-        public const string FakeConnectionString = "Endpoint=http://localhost;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;Port=8080;Version=1.0";
+        public const string FakeConnectionString = "Endpoint=http://fake;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;Port=8080;Version=1.0";
 
         private static readonly string[] ConnectionStringKeyPrefixs = new string[] { Constants.Keys.ConnectionStringKeyPrefix, Constants.Keys.ConnectionStringSecondaryKeyPrefix };
 
@@ -22,13 +24,13 @@ namespace Microsoft.Azure.SignalR.Tests
             {":secondary",(string.Empty,EndpointType.Secondary) }
         };
 
-        public static IEnumerable<object[]> ParseConnectionStringData = from section in ConnectionStringKeyPrefixs
+        public static IEnumerable<object[]> ParseServiceEndpointData = from section in ConnectionStringKeyPrefixs
                                                                         from tuple in EndpointDict
                                                                         select new object[] { section + tuple.Key, tuple.Value.Item1, tuple.Value.Item2 };
 
         [Theory]
-        [MemberData(nameof(ParseConnectionStringData))]
-        public void ParseConnectionStringTest(string key, string endpointName, EndpointType type)
+        [MemberData(nameof(ParseServiceEndpointData))]
+        public void ParseServiceEndpointTest(string key, string endpointName, EndpointType type)
         {
             IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
             configuration[key] = FakeConnectionString;
@@ -39,6 +41,24 @@ namespace Microsoft.Azure.SignalR.Tests
             var resultEndpoint = options.Endpoints.Single();
             Assert.Equal(endpointName, resultEndpoint.Name);
             Assert.Equal(type, resultEndpoint.EndpointType);
+        }
+
+        [Fact]
+        public void ParseMultipleEndpointsTest(){
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
+            var defaultConnectionString = "Endpoint=http://default;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;Port=8080;Version=1.0";
+            configuration[$"{Constants.Keys.ConnectionStringDefaultKey}"] = defaultConnectionString;
+            foreach(var key in EndpointDict.Keys){
+                configuration[ConfigurationPath.Combine(Constants.Keys.ConnectionStringDefaultKey, key)] = FakeConnectionString;
+            }
+
+            var setup = new ServiceOptionsSetup(configuration);
+            var options = new ServiceOptions();
+            setup.Configure(options);
+
+
+            Assert.Equal(defaultConnectionString, options.ConnectionString);
+            Assert.Equal(EndpointDict.AsEnumerable().Count(), options.Endpoints.Length);
         }
     }
 }
