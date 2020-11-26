1bd02e9dde11b57511f06a602d23b4ce5c252bbf
Zitong Yang <zityang@microsoft.com>
2020-11-26 13:35:33 +0800

    fix

--- src/Microsoft.Azure.SignalR.Management/Obsolete/ServiceManager.cs
+++ src/Microsoft.Azure.SignalR.Management/Obsolete/ServiceManager.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.SignalR.Management
 
         public void Dispose()
         {
-            (_serviceProvider as IDisposable).Dispose();
+            (_serviceProvider as IDisposable)?.Dispose();
         }
 
         public string GenerateClientAccessToken(string hubName, string userId = null, IList<Claim> claims = null, TimeSpan? lifeTime = null)
