f1caf75983a324fff84c24ea83cab659407955b1
Zitong Yang <zityang@microsoft.com>
2020-11-30 14:56:56 +0800

    allow IServiceProvider to be not disposable

--- src/Microsoft.Azure.SignalR.Management/ServiceContext.cs
+++ src/Microsoft.Azure.SignalR.Management/ServiceContext.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -36,7 +36,7 @@ namespace Microsoft.Azure.SignalR.Management
 
         public void Dispose()
         {
-            ((IDisposable)_serviceProvider).Dispose();
+            (_serviceProvider as IDisposable).Dispose();
         }
     }
 }
