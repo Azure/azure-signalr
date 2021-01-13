bfd8aa61ff0109496bdc628281845adf0494cfe2
yzt <zityang@microsoft.com>
2021-01-13 19:07:01 +0800

    Enable negotiation for multiple endpoints inside ServiceHubContext (#1182)
    * Enable negotiation for multiple endpoints inside ServiceHubContext
    * Allow connectionstring to be null when service endpoints not null.
    * Remove unused code.
    * move `IServiceManager` etc. files out of "obsolete" directory.

--- src/Microsoft.Azure.SignalR.Management/ServiceHubContext.cs
+++ src/Microsoft.Azure.SignalR.Management/ServiceHubContext.cs
@@ -2,16 +2,23 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.SignalR.Management
 {
-    internal class ServiceHubContext : IServiceHubContext
+    internal class ServiceHubContext : IInternalServiceHubContext
     {
+        private readonly string _hubName;
         private readonly IHubContext<Hub> _hubContext;
         private readonly IServiceHubLifetimeManager _lifetimeManager;
+        private readonly NegotiateProcessor _negotiateProcessor;
+        private readonly IServiceEndpointManager _endpointManager;
 
         internal IServiceProvider ServiceProvider { get; }
 
@@ -21,13 +28,23 @@ namespace Microsoft.Azure.SignalR.Management
 
         public IUserGroupManager UserGroups { get; }
 
-        public ServiceHubContext(IHubContext<Hub> hubContext, IServiceHubLifetimeManager lifetimeManager, IServiceProvider serviceProvider)
+        public ServiceHubContext(string hubName, IHubContext<Hub> hubContext, IServiceHubLifetimeManager lifetimeManager, IServiceProvider serviceProvider, NegotiateProcessor negotiateProcessor, IServiceEndpointManager endpointManager)
         {
+            _hubName = hubName;
             _hubContext = hubContext;
             _lifetimeManager = lifetimeManager;
             UserGroups = new UserGroupsManager(lifetimeManager);
             ServiceProvider = serviceProvider;
+            _negotiateProcessor = negotiateProcessor;
+            _endpointManager = endpointManager;
         }
+
+        Task<NegotiationResponse> IInternalServiceHubContext.NegotiateAsync(HttpContext httpContext, string userId, IList<Claim> claims, TimeSpan? lifetime, bool isDiagnosticClient, CancellationToken cancellationToken)
+        {
+            return _negotiateProcessor.NegotiateAsync(_hubName, httpContext, userId, claims, lifetime, isDiagnosticClient, cancellationToken);
+        }
+
+        IEnumerable<ServiceEndpoint> IInternalServiceHubContext.GetServiceEndpoints() => _endpointManager.GetEndpoints(_hubName);
 
         public async Task DisposeAsync()
         {
