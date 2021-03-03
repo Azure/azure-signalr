752dd4f2036a26e96d5a68a077a95d57b82a9bbc
Zitong Yang <zityang@microsoft.com>
2021-03-03 16:29:55 +0800

    Add unit test

---
+++ test/Microsoft.Azure.SignalR.Management.Tests/TestClasses/MessageVerifiableConnection.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Microsoft.Azure.SignalR.Protocol;
+using Microsoft.Azure.SignalR.Tests.Common;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.SignalR.Management.Tests
+{
+    internal class MessageVerifiableConnection : TestServiceConnection
+    {
+        public ConcurrentQueue<ServiceMessage> ReceivedMessages { get; } = new();
+
+        public MessageVerifiableConnection(ServiceConnectionStatus status = ServiceConnectionStatus.Connected,
+            ILogger logger = null,
+            IServiceMessageHandler serviceMessageHandler = null,
+            IServiceEventHandler serviceEventHandler = null) : base(status, false, logger, serviceMessageHandler, serviceEventHandler)
+        {
+        }
+
+        protected override Task<bool> SafeWriteAsync(ServiceMessage serviceMessage)
+        {
+            ReceivedMessages.Enqueue(serviceMessage);
+            return Task.FromResult(true);
+        }
+    }
+}
