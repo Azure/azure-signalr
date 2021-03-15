93902d5aa1939792c8233911483445cd0e954555
Zitong Yang <zityang@microsoft.com>
2021-03-15 18:35:47 +0800

    Not allow transient mode to configure service endpoints

--- src/Microsoft.Azure.SignalR.Management/Configuration/ServiceManagerOptions.cs
+++ src/Microsoft.Azure.SignalR.Management/Configuration/ServiceManagerOptions.cs
@@ -55,6 +55,17 @@ namespace Microsoft.Azure.SignalR.Management
             {
                 throw new InvalidOperationException($"{nameof(ServiceEndpoints)} is empty. {nameof(ConnectionString)} is  null, empty, or consists only of white-space.");
             }
+            if(ServiceTransportType == ServiceTransportType.Transient)
+            {
+                if (string.IsNullOrWhiteSpace(ConnectionString))
+                {
+                    throw new InvalidOperationException($"{nameof(ConnectionString)} must be set for transient mode.");
+                }
+                if(ServiceEndpoints!= null && ServiceEndpoints.Length > 0)
+                {
+                    throw new NotImplementedException($"Multiple endpoints are not supported for persistent mode. Please unset {nameof(ServiceEndpoints)}");
+                }
+            }
         }
     }
 }
