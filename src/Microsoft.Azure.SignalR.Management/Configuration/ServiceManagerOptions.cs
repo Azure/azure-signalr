c7be4a7141341e20d87aded1bdfad3540cedf0ad
Wanpeng Li <wanl@microsoft.com>
2021-06-28 16:22:44 +0800

    add EnableMessageTracing to ServiceManagerOptions  (#1330)
    * update
    
    * simplify code

--- src/Microsoft.Azure.SignalR.Management/Configuration/ServiceManagerOptions.cs
+++ src/Microsoft.Azure.SignalR.Management/Configuration/ServiceManagerOptions.cs
@@ -48,6 +48,11 @@ namespace Microsoft.Azure.SignalR.Management
         /// </summary>
         public JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings();
 
+        /// <summary>
+        /// Gets or sets a value indicating whether message tracing ID is append to messages.
+        /// </summary>
+        public bool EnableMessageTracing { get; set; } = false;
+
         internal string ProductInfo { get; set; }
 
         internal void ValidateOptions()
