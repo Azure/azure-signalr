44d7f0aaad1cb816aed4323677858701e486eb62
wanl <wanl@microsoft.com>
2021-07-07 14:23:44 +0800

    rm

--- src/Microsoft.Azure.SignalR.Management/TaskExtensions.cs
+++ src/Microsoft.Azure.SignalR.Management/TaskExtensions.cs
@@ -2,10 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.Azure.SignalR.Management
 {
