a2448248eda94526ca698c4e3a2e6da7a8ddb34e
stdrickforce <stdrickforce@gmail.com>
2021-08-18 21:10:43 +0800

    add constructor for aad keys

--- test/Microsoft.Azure.SignalR.Common.Tests/ServiceEndpointFacts.cs
+++ test/Microsoft.Azure.SignalR.Common.Tests/ServiceEndpointFacts.cs
@@ -1,35 +1,17 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+
+using Azure.Identity;
+
 using Xunit;
 
 namespace Microsoft.Azure.SignalR.Common.Tests
 {
     public class ServiceEndpointFacts
     {
-        [Theory]
-        [InlineData("a", "a", EndpointType.Primary)]
-        [InlineData("a:primary", "a", EndpointType.Primary)]
-        [InlineData("secondary", "secondary", EndpointType.Primary)]
-        [InlineData("a:secondary", "a", EndpointType.Secondary)]
-        [InlineData(":secondary", "", EndpointType.Secondary)]
-        internal void TestParseKey(string key, string expectedName, EndpointType expectedType)
-        {
-            var (name, type) = ServiceEndpoint.ParseKey(key);
-
-            Assert.Equal(expectedName, name);
-            Assert.Equal(expectedType, type);
-        }
-
-        [Theory]
-        [MemberData(nameof(TestEndpointsEqualityInput))]
-        internal void TestEndpointsEquality(ServiceEndpoint first, ServiceEndpoint second, bool equal)
-        {
-            Assert.Equal(first.Equals(second), equal);
-            Assert.Equal(first.GetHashCode() == second.GetHashCode(), equal);
-        }
-
         public static IEnumerable<object[]> TestEndpointsEqualityInput = new object[][]
         {
             new object[]
@@ -93,5 +75,73 @@ namespace Microsoft.Azure.SignalR.Common.Tests
                 true,
             }
         };
+
+        [Theory]
+        [InlineData("http://localhost", "http://localhost", 80)]
+        [InlineData("https://localhost", "https://localhost", 443)]
+        [InlineData("http://localhost:5050", "http://localhost", 5050)]
+        [InlineData("https://localhost:5050", "https://localhost", 5050)]
+        public void TestAadConstructor(string url, string endpoint, int port)
+        {
+            var uri = new Uri(url);
+            var serviceEndpoint = new ServiceEndpoint(uri, new DefaultAzureCredential());
+            Assert.IsType<AadAccessKey>(serviceEndpoint.AccessKey);
+            Assert.Equal(endpoint, serviceEndpoint.Endpoint);
+            Assert.Equal(port, serviceEndpoint.Port);
+            Assert.Equal("", serviceEndpoint.Name);
+            Assert.Equal(EndpointType.Primary, serviceEndpoint.EndpointType);
+            TestCopyConstructor(serviceEndpoint);
+        }
+
+        [Theory]
+        [InlineData("ftp://localhost")]
+        [InlineData("ws://localhost")]
+        [InlineData("localhost:5050")]
+        public void TestAadConstructorThrowsError(string url)
+        {
+            var uri = new Uri(url);
+            Assert.Throws<ArgumentException>(() => new ServiceEndpoint(uri, new DefaultAzureCredential()));
+        }
+
+        [Theory]
+        [InlineData("", "", EndpointType.Primary)]
+        [InlineData("foo", "foo", EndpointType.Primary)]
+        [InlineData("foo:primary", "foo", EndpointType.Primary)]
+        [InlineData("foo:secondary", "foo", EndpointType.Secondary)]
+        [InlineData("foo:SECONDARY", "foo", EndpointType.Secondary)]
+        [InlineData("foo:bar", "foo:bar", EndpointType.Primary)]
+        [InlineData(":", ":", EndpointType.Primary)]
+        [InlineData(":bar", ":bar", EndpointType.Primary)]
+        [InlineData(":primary", "", EndpointType.Primary)]
+        [InlineData(":secondary", "", EndpointType.Secondary)]
+        public void TestAadConstructorWithKey(string key, string name, EndpointType type)
+        {
+            var uri = new Uri("http://localhost");
+            var serviceEndpoint = new ServiceEndpoint(key, uri, new DefaultAzureCredential());
+            Assert.IsType<AadAccessKey>(serviceEndpoint.AccessKey);
+            Assert.Equal(name, serviceEndpoint.Name);
+            Assert.Equal(type, serviceEndpoint.EndpointType);
+            TestCopyConstructor(serviceEndpoint);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestEndpointsEqualityInput))]
+        public void TestEndpointsEquality(ServiceEndpoint first, ServiceEndpoint second, bool equal)
+        {
+            Assert.Equal(first.Equals(second), equal);
+            Assert.Equal(first.GetHashCode() == second.GetHashCode(), equal);
+        }
+
+        private static void TestCopyConstructor(ServiceEndpoint endpoint)
+        {
+            var other = new ServiceEndpoint(endpoint);
+            Assert.Equal(endpoint.Name, other.Name);
+            Assert.Equal(endpoint.EndpointType, other.EndpointType);
+            Assert.Equal(endpoint.Endpoint, other.Endpoint);
+            Assert.Equal(endpoint.Port, other.Port);
+            Assert.Equal(endpoint.ClientEndpoint, other.ClientEndpoint);
+            Assert.Equal(endpoint.Version, other.Version);
+            Assert.Equal(endpoint.AccessKey, other.AccessKey);
+        }
     }
 }
