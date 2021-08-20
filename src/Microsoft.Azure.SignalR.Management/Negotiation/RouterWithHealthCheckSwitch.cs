a109315259fb477f9d696aea425f1698090f1495
Zitong Yang <zityang@microsoft.com>
2021-08-20 17:28:48 +0800

    Disable endpoint online check from `IEndpointRouter` side for single endpoint in Serverless SDK
    This affects both persistent mode and transient mode

---
+++ src/Microsoft.Azure.SignalR.Management/Negotiation/RouterWithHealthCheckSwitch.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Azure.SignalR.Management
+{
+    /// <summary>
+    /// An endpoint router that only checks health when there are multiple endpoints.
+    /// </summary>
+    internal class AutoHealthCheckRouter : EndpointRouterDecorator
+    {
+        public override ServiceEndpoint GetNegotiateEndpoint(HttpContext context, IEnumerable<ServiceEndpoint> endpoints)
+        {
+            return endpoints.Count() == 1 ? endpoints.Single() : base.GetNegotiateEndpoint(context, endpoints);
+        }
+    }
+}
