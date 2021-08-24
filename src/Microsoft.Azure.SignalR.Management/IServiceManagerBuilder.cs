ffb264fe727008fc836088fad4e653afd3603195
yzt <zityang@microsoft.com>
2021-08-24 17:51:16 +0800

    Deprecate `IServiceManagerBuilder` method (#1406)

--- src/Microsoft.Azure.SignalR.Management/IServiceManagerBuilder.cs
+++ src/Microsoft.Azure.SignalR.Management/IServiceManagerBuilder.cs
@@ -1,17 +1,21 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.Azure.SignalR.Management
 {
     /// <summary>
     /// A builder abstraction for configuring <see cref="IServiceManager"/> instances.
     /// </summary>
+    [Obsolete("Use ServiceManagerBuilder.BuildServiceManager() to build an abstract class of ServiceManager instead.")]
     public interface IServiceManagerBuilder
     {
         /// <summary>
         /// Builds <see cref="IServiceManager"/> instances.
         /// </summary>
         /// <returns>The instance of the <see cref="IServiceManager"/>.</returns>
+        [Obsolete("Use ServiceManagerBuilder.BuildServiceManager() instead.")]
         IServiceManager Build();
     }
 }
