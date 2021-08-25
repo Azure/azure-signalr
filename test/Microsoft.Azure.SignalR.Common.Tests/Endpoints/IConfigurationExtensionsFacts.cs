2dd5cbf0b3bb558fdd20a90c28e657d70d6fbb5b
Zitong Yang <zityang@microsoft.com>
2021-08-25 17:56:45 +0800

    Add methods to parse `ServiceEndpoint` from configuration based on identity
    Now support following format (identity-based + connection string):
    ```json
    {
        "<SectionNameForEndpoints>": {
            "eastus": {
                "serviceUri": "http://signalr.service.uri.com:441",
                "credential": "managedidentity",
                "type": "secondary"
            },
            "westus": {
                "primary": "<ConnectionString>",
                "secondary": "<ConnectionString>"
            },
            "southasia": "<ConnectionString>"
        }
    }
    ```

--- test/Microsoft.Azure.SignalR.Common.Tests/Endpoints/IConfigurationExtensionsFacts.cs
+++ test/Microsoft.Azure.SignalR.Common.Tests/Endpoints/IConfigurationExtensionsFacts.cs
@@ -2,28 +2,105 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Linq;
+using Azure.Identity;
 using Microsoft.Azure.SignalR.Tests.Common;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Microsoft.Azure.SignalR.Common.Tests
 {
     public class IConfigurationExtensionsFacts
     {
-        [Theory]
-        [InlineData("a")]
-        [InlineData("a:primary")]
-        [InlineData("secondary")]
-        [InlineData("a:secondary")]
-        [InlineData(":secondary")]
-        public void GetEndpointsWithSuffix(string relativePath)
+        [Fact]
+        public void TestGetNamedEndpointFromIdentityWithNoUri()
+        {
+            var services = new ServiceCollection();
+            services.AddAzureClientsCore();
+            var factory = services.BuildServiceProvider().GetRequiredService<AzureComponentFactory>();
+            var config = new ConfigurationBuilder().AddInMemoryCollection().Build();
+            Assert.Null(config.GetSection("eastus").GetNamedEndpointFromIdentity(factory));
+        }
+
+        [Fact]
+        public void TestGetNamedEndpointFromIdentityWithOnlyUri()
+        {
+            var services = new ServiceCollection();
+            services.AddAzureClientsCore();
+            var factory = services.BuildServiceProvider().GetRequiredService<AzureComponentFactory>();
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection().Build();
+            var uri = "http://signalr.service.uri.com:441";
+            config["eastus:serviceUri"] = uri;
+
+            var endpoint = config.GetSection("eastus").GetNamedEndpointFromIdentity(factory);
+            Assert.NotNull(endpoint);
+            Assert.Equal("eastus", endpoint.Name);
+            Assert.Equal(uri, endpoint.Endpoint);
+            Assert.IsType<DefaultAzureCredential>((endpoint.AccessKey as AadAccessKey).TokenCredential);
+            Assert.Equal(EndpointType.Primary, endpoint.EndpointType);
+        }
+
+        [Fact]
+        public void TestGetNamedEndpointFromIdentityWithAllEndpointField()
+        {
+            var services = new ServiceCollection();
+            services.AddAzureClientsCore();
+            var factory = services.BuildServiceProvider().GetRequiredService<AzureComponentFactory>();
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection().Build();
+            var uri = "http://signalr.service.uri.com:441";
+            config["eastus:serviceUri"] = uri;
+            config["eastus:credential"] = "managedidentity";
+            config["eastus:type"] = "secondary";
+
+            var endpoint = config.GetSection("eastus").GetNamedEndpointFromIdentity(factory);
+            Assert.NotNull(endpoint);
+            Assert.Equal("eastus", endpoint.Name);
+            Assert.Equal(uri, endpoint.Endpoint);
+            Assert.IsType<ManagedIdentityCredential>((endpoint.AccessKey as AadAccessKey).TokenCredential);
+            Assert.Equal(EndpointType.Secondary, endpoint.EndpointType);
+        }
+
+        [Fact]
+        public void TestGetNamedEndpointsFromConnectionString()
         {
             var connectionString = FakeEndpointUtils.GetFakeConnectionString(1).Single();
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
-            var connectionStringKey = "key";
-            configuration[connectionStringKey] = connectionString;
-            configuration[$"{connectionStringKey}:{relativePath}"] = connectionString;
-            Assert.Single(configuration.GetEndpoints(connectionStringKey));
+            var config = new ConfigurationBuilder().AddInMemoryCollection().Build();
+            config["eastus"] = connectionString;
+            config["eastus:primary"] = connectionString;
+            config["eastus:secondary"] = connectionString;
+            var endpoints = config.GetSection("eastus").GetNamedEndpointsFromConnectionString().ToArray();
+            Assert.Equal(3, endpoints.Length);
+            Assert.All(endpoints, e => Assert.Equal("eastus", e.Name));
+            Assert.Equal(EndpointType.Primary, endpoints[0].EndpointType);
+            Assert.Equal(EndpointType.Primary, endpoints[1].EndpointType);
+            Assert.Equal(EndpointType.Secondary, endpoints[2].EndpointType);
+        }
+
+        [Fact]
+        public void TestGetEndpointsFromIdentityAndConnectionString()
+        {
+            var services = new ServiceCollection();
+            services.AddAzureClientsCore();
+            var factory = services.BuildServiceProvider().GetRequiredService<AzureComponentFactory>();
+            var config = new ConfigurationBuilder().AddInMemoryCollection().Build();
+
+            var serviceUri = "http://signalr.service.uri.com:441";
+            config["endpoints:eastus:serviceUri"] = serviceUri;
+            config["endpoints:westus:secondary"] = FakeEndpointUtils.GetFakeConnectionString(1).Single();
+
+            var endpoints = config.GetSection("endpoints").GetEndpoints(factory).ToArray();
+            Assert.Collection(endpoints, e =>
+            {
+                Assert.Equal("eastus", e.Name);
+                Assert.Equal(serviceUri, e.Endpoint);
+            }, e =>
+            {
+                Assert.Equal("westus", e.Name);
+                Assert.Equal(EndpointType.Secondary, e.EndpointType);
+            });
         }
     }
 }
