424fd70f048084567fb9f3f44d5dc8e89cce0f6c
Zitong Yang <zityang@microsoft.com>
2021-09-03 14:45:13 +0800

    Revert "Add methods to parse `ServiceEndpoint` from configuration based on identity (#1413)"
    This reverts commit e5a69b20daf7f7aa2786ff59df61433c4050c6ab.
    Azure SDK thinks no need to support parsing a `TokenCredential` from a configuration in SDK. Just a constructor accepts `TokenCredential` is enough.

--- src/Microsoft.Azure.SignalR.Common/Endpoints/IConfigurationExtension.cs
+++ src/Microsoft.Azure.SignalR.Common/Endpoints/IConfigurationExtension.cs
@@ -1,72 +1,22 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Azure.SignalR
 {
     internal static class IConfigurationExtension
     {
+        /// <param name="configuration"></param>
+        /// <param name="sectionName"></param>
         public static IEnumerable<ServiceEndpoint> GetEndpoints(this IConfiguration configuration, string sectionName)
         {
             var section = configuration.GetSection(sectionName);
             return section.AsEnumerable(true)
                           .Where(entry => !string.IsNullOrEmpty(entry.Value))
                           .Select(entry => new ServiceEndpoint(entry.Key, entry.Value));
-        }
-
-        public static IEnumerable<ServiceEndpoint> GetEndpoints(this IConfiguration config, AzureComponentFactory azureComponentFactory)
-        {
-            foreach (var child in config.GetChildren())
-            {
-                if (child.TryGetNamedEndpointFromIdentity(azureComponentFactory,out var serviceEndpoint))
-                {
-                    yield return serviceEndpoint;
-                }
-                else
-                {
-                    foreach(var endpoint in GetNamedEndpointsFromConnectionString(child))
-                    {
-                        yield return endpoint;
-                    }
-                }
-            }
-        }
-
-        public static IEnumerable<ServiceEndpoint> GetNamedEndpointsFromConnectionString(this IConfigurationSection section)
-        {
-            var endpointName = section.Key;
-            if (section.Value != null)
-            {
-                yield return new ServiceEndpoint(section.Key, section.Value);
-            }
-            //This part isn't responsible for deduplicating.
-            if (section["primary"] != null)
-            {
-                yield return new ServiceEndpoint(section["primary"], EndpointType.Primary, endpointName);
-            }
-            if (section["secondary"] != null) {
-                yield return new ServiceEndpoint(section["secondary"], EndpointType.Secondary, endpointName);
-            }
-        }
-
-        public static bool TryGetNamedEndpointFromIdentity(this IConfigurationSection section, AzureComponentFactory azureComponentFactory, out ServiceEndpoint endpoint)
-        {
-            var uri = section[Constants.Keys.ServiceUriKey];
-            if (uri != null)
-            {
-                var name = section.Key;
-                var type = section.GetValue(Constants.Keys.EndpointTypeKey, EndpointType.Primary);
-                var credential = azureComponentFactory.CreateTokenCredential(section);
-                endpoint = new ServiceEndpoint(new Uri(uri), credential, type, name);
-                return true;
-            }
-            endpoint = null;
-            return false ;
         }
     }
 }
