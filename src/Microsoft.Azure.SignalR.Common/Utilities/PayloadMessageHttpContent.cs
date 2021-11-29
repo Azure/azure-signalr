aded80768219a20a2166955f08771c0968db2a91
Zitong Yang <zityang@microsoft.com>
2021-11-29 16:20:12 +0800

    Prepare to serialize payload with `ObjectSerializer`

---
+++ src/Microsoft.Azure.SignalR.Common/Utilities/PayloadMessageHttpContent.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Azure.Core.Serialization;
+
+namespace Microsoft.Azure.SignalR
+{
+    internal class PayloadMessageContent : HttpContent
+    {
+        private readonly PayloadMessage _payloadMessage;
+        private readonly ObjectSerializer _jsonObjectSerializer;
+
+        public PayloadMessageContent(PayloadMessage payloadMessage, ObjectSerializer jsonObjectSerializer)
+        {
+            _payloadMessage = payloadMessage;
+            _jsonObjectSerializer = jsonObjectSerializer;
+            Headers.ContentType = new MediaTypeHeaderValue("application/json")
+            {
+                CharSet = "utf-8"
+            };
+        }
+
+        protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            var reusableUtf8JsonWriter = ReusableUtf8JsonWriter.Get(stream);
+            var jsonWriter = reusableUtf8JsonWriter.GetJsonWriter();
+            jsonWriter.WriteStartObject();
+            jsonWriter.WriteString(nameof(PayloadMessage.Target), _payloadMessage.Target);
+            jsonWriter.WritePropertyName(nameof(PayloadMessage.Arguments));
+            await jsonWriter.FlushAsync();
+            await _jsonObjectSerializer.SerializeAsync(stream, _payloadMessage.Arguments, typeof(object[]), default);
+            jsonWriter.WriteEndObject();
+            await jsonWriter.FlushAsync();
+            ReusableUtf8JsonWriter.Return(reusableUtf8JsonWriter);
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = 0;
+            return false;
+        }
+    }
+}
--- src/Microsoft.Azure.SignalR.Common/Utilities/ReusableUtf8JsonWriter.cs
+++ src/Microsoft.Azure.SignalR.Common/Utilities/ReusableUtf8JsonWriter.cs
@@ -1,75 +1,74 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Buffers;
+using System.IO;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 
-namespace Microsoft.AspNetCore.Internal;
-
-# nullable enable
-
-/// <summary>
-/// Copied from https://github.com/dotnet/aspnetcore/blob/d9660d157627af710b71c636fa8cb139616cadba/src/SignalR/common/Shared/ReusableUtf8JsonWriter.cs
-/// </summary>
-internal sealed class ReusableUtf8JsonWriter
+#nullable enable
+namespace Microsoft.Azure.SignalR
 {
-    [ThreadStatic]
-    private static ReusableUtf8JsonWriter? _cachedInstance;
+    /// <summary>
+    /// Modified based on https://github.com/dotnet/aspnetcore/blob/d9660d157627af710b71c636fa8cb139616cadba/src/SignalR/common/Shared/ReusableUtf8JsonWriter.cs
+    /// </summary>
+    internal sealed class ReusableUtf8JsonWriter
+    {
+        [ThreadStatic]
+        private static ReusableUtf8JsonWriter? _cachedInstance;
 
-    private readonly Utf8JsonWriter _writer;
+        private readonly Utf8JsonWriter _writer;
 
 #if DEBUG
-    private bool _inUse;
+        private bool _inUse;
 #endif
 
-    public ReusableUtf8JsonWriter(IBufferWriter<byte> stream)
-    {
-        _writer = new Utf8JsonWriter(stream, new JsonWriterOptions()
+        public ReusableUtf8JsonWriter(Stream stream)
         {
-#if !DEBUG
+            _writer = new Utf8JsonWriter(stream, new JsonWriterOptions()
+            {
+                // We must skip validation because what we break the writing midway and write JSON in other ways.
                 SkipValidation = true,
-#endif
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-        });
-    }
-
-    public static ReusableUtf8JsonWriter Get(IBufferWriter<byte> stream)
-    {
-        var writer = _cachedInstance;
-        if (writer == null)
-        {
-            writer = new ReusableUtf8JsonWriter(stream);
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            });
         }
 
-        // Taken off the thread static
-        _cachedInstance = null;
-#if DEBUG
-        if (writer._inUse)
+        public static ReusableUtf8JsonWriter Get(Stream stream)
         {
-            throw new InvalidOperationException("The writer wasn't returned!");
+            var writer = _cachedInstance;
+            if (writer == null)
+            {
+                writer = new ReusableUtf8JsonWriter(stream);
+            }
+
+            // Taken off the thread static
+            _cachedInstance = null;
+#if DEBUG
+            if (writer._inUse)
+            {
+                throw new InvalidOperationException("The writer wasn't returned!");
+            }
+
+            writer._inUse = true;
+#endif
+            writer._writer.Reset(stream);
+            return writer;
         }
 
-        writer._inUse = true;
-#endif
-        writer._writer.Reset(stream);
-        return writer;
-    }
+        public static void Return(ReusableUtf8JsonWriter writer)
+        {
+            _cachedInstance = writer;
 
-    public static void Return(ReusableUtf8JsonWriter writer)
-    {
-        _cachedInstance = writer;
-
-        writer._writer.Reset();
+            writer._writer.Reset();
 
 #if DEBUG
-        writer._inUse = false;
+            writer._inUse = false;
 #endif
-    }
+        }
 
-    public Utf8JsonWriter GetJsonWriter()
-    {
-        return _writer;
+        public Utf8JsonWriter GetJsonWriter()
+        {
+            return _writer;
+        }
     }
 }
