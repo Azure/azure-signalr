e4be641d7ec937cb692913831f0791493c021482
yzt <zityang@microsoft.com>
2021-12-15 13:41:49 +0800

    Make `ServiceManagerOptions.ObjectSerializer` effective for persistent mode (#1506)

--- src/Microsoft.Azure.SignalR.Management/Serialization/SerializationDependencyInjectionExtensions.cs
+++ src/Microsoft.Azure.SignalR.Management/Serialization/SerializationDependencyInjectionExtensions.cs
@@ -3,7 +3,6 @@
 
 using System;
 using Azure.Core.Serialization;
-using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.SignalR.Management
@@ -12,21 +11,9 @@ namespace Microsoft.Azure.SignalR.Management
     {
         public static IServiceCollection AddNewtonsoftHubProtocol(this IServiceCollection services, Action<NewtonsoftServiceHubProtocolOptions> configure)
         {
-            // for persistent transport type only:
-            services.Configure(configure);
-#if NETCOREAPP3_0_OR_GREATER
-            services.AddSingleton<IHubProtocol, NewtonsoftJsonHubProtocol>();
-            services.ConfigureOptions<NewtonsoftHubProtocolOptionsSetup>();
-#endif
-#if NETSTANDARD2_0
-            services.ConfigureOptions<JsonHubProtocolOptionsSetup>();
-#endif
-
-            // for transient transport type only, will apply to persistent transport type later.
             var options = new NewtonsoftServiceHubProtocolOptions();
             configure?.Invoke(options);
             services.Configure<ServiceManagerOptions>(o => o.ObjectSerializer = new NewtonsoftJsonObjectSerializer(options.PayloadSerializerSettings));
-
             return services;
         }
     }
