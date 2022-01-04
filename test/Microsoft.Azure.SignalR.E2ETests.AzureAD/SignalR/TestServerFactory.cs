0af4139262f72ce18ca606a7804bdc2ad6242640
stdrickforce <stdrickforce@gmail.com>
2022-01-04 18:13:14 +0800

    add e2e test cases for AzureAD

---
+++ test/Microsoft.Azure.SignalR.E2ETests.AzureAD/SignalR/TestServerFactory.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.SignalR.Tests.Common;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.SignalR.Tests
+{
+    internal class TestServerFactory : ITestServerFactory
+    {
+        public ITestServer Create(ITestOutputHelper output)
+        {
+            return new TestServer(output);
+        }
+    }
+}
---
+++ test/Microsoft.Azure.SignalR.Tests.Common/SkipIfEndpointNotPresentAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.AspNetCore.Testing.xunit;
+
+namespace Microsoft.Azure.SignalR.Tests.Common
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class SkipIfEndpointNotPresentAttribute: Attribute, ITestCondition
+    {
+        public bool IsMet => IsConnectionStringAvailable();
+
+        public string SkipReason => "endpoint is not present.";
+
+        private static bool IsConnectionStringAvailable()
+        {
+            return !string.IsNullOrEmpty(TestConfiguration.Instance.TestEndpoint);
+        }
+    }
+}
