e33aa2f3768d65f713b396b68531c2428c3314e8
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2022-08-29 17:04:38 +0800

    update

--- test/Microsoft.Azure.SignalR.Management.Tests/Serialization/SerailizerFacts.cs
+++ test/Microsoft.Azure.SignalR.Management.Tests/Serialization/SerailizerFacts.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Azure.Core.Serialization;
 using Microsoft.AspNetCore.SignalR;
