790b6c06ae5a6580dfaa55a0286277b00f9c3e3d
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2022-09-23 15:18:01 +0800

    bug fix

--- src/Microsoft.Azure.SignalR.Common/ClientInvocation/ClientInvocationManager.cs
+++ src/Microsoft.Azure.SignalR.Common/ClientInvocation/ClientInvocationManager.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if NET7_0_OR_GREATER
 using System;
+#if NET7_0_OR_GREATER
 using Microsoft.AspNetCore.SignalR;
 #endif
 
@@ -13,8 +13,8 @@ namespace Microsoft.Azure.SignalR
         public IClientResultsManager Caller => _clientResultsManager;
         public IRoutedClientResultsManager Router => _routedClientResultsManager;
 
-        private readonly IClientResultsManager _clientResultsManager;
-        private readonly IRoutedClientResultsManager _routedClientResultsManager;
+        private readonly IClientResultsManager _clientResultsManager = null;
+        private readonly IRoutedClientResultsManager _routedClientResultsManager = null;
 
 #if NET7_0_OR_GREATER
         ClientInvocationManager(IHubProtocolResolver hubProtocolResolver)
@@ -23,7 +23,7 @@ namespace Microsoft.Azure.SignalR
         }
 #else
         ClientInvocationManager()
-        {
+        { 
             throw new NotImplementedException();
         }
 #endif
