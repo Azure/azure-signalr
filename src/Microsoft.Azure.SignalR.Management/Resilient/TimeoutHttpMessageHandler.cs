2d7f622c523c194f7f7b43697a1dce8eb6423cac
Zitong Yang <zityang@microsoft.com>
2023-10-19 16:17:20 +0800

    fix comment

--- src/Microsoft.Azure.SignalR.Management/Resilient/TimeoutHttpMessageHandler.cs
+++ src/Microsoft.Azure.SignalR.Management/Resilient/TimeoutHttpMessageHandler.cs
@@ -13,6 +13,7 @@ namespace Microsoft.Azure.SignalR.Management;
 
 internal class TimeoutHttpMessageHandler : DelegatingHandler
 {
+    private readonly bool _enableTimeout = false;
     private readonly TimeSpan _timeout;
     public TimeoutHttpMessageHandler(IOptions<ServiceManagerOptions> serviceManagerOptions)
     {
@@ -25,23 +26,24 @@ internal class TimeoutHttpMessageHandler : DelegatingHandler
         else
         {
             _timeout = options.HttpClientTimeout;
+            _enableTimeout = true;
         }
     }
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        if (_timeout == Timeout.InfiniteTimeSpan)
+        if (_enableTimeout)
         {
-            return await base.SendAsync(request, cancellationToken);
+            var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            cts.CancelAfter(_timeout);
+            try
+            {
+                return await base.SendAsync(request, cts.Token);
+            }
+            catch (OperationCanceledException ex) when (!cancellationToken.IsCancellationRequested)
+            {
+                throw new TaskCanceledException($"The request was canceled due to the configured HttpClient.Timeout of {_timeout.TotalSeconds} seconds elapsing.", new TimeoutException(ex.Message, ex));
+            }
         }
-        var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-        cts.CancelAfter(_timeout);
-        try
-        {
-            return await base.SendAsync(request, cts.Token);
-        }
-        catch (OperationCanceledException ex) when (!cancellationToken.IsCancellationRequested)
-        {
-            throw new TaskCanceledException($"The request was canceled due to the configured HttpClient.Timeout of {_timeout.TotalSeconds} seconds elapsing.", new TimeoutException(ex.Message, ex));
-        }
+        return await base.SendAsync(request, cancellationToken);
     }
 }
