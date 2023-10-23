b39b52d280a62c7bfcfe0e1628cac7fa1ce49847
stdrickforce <stdrickforce@gmail.com>
2023-10-23 11:10:17 +0800

    Add MigrateConnectionWithAckMessage

---
+++ src/Microsoft.Azure.SignalR.Protocols/ConnectionWithAckMessage.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.SignalR.Protocol
+{
+    public class ConnectionWithAckMessage
+    {
+        public class MigrateConnectionWithAckMessage : ExtensibleServiceMessage, IAckableMessage
+        {
+            /// <summary>
+            /// Gets or sets the ack Id.
+            /// </summary>
+            public int AckId { get; set; }
+
+            /// <summary>
+            /// Gets or sets the connection Id.
+            /// </summary>
+            public string ConnectionId { get; set; }
+        }
+    }
+}
