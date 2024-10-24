cd4eb4e6cf870c8d938def49ccb8fd8918d5412e
stdrickforce <stdrickforce@gmail.com>
2024-10-24 15:01:40 +0800

    update

--- src/Microsoft.Azure.SignalR.Common/Auth/MicrosoftEntra/AccessKeyResponse.cs
+++ src/Microsoft.Azure.SignalR.Common/Auth/MicrosoftEntra/AccessKeyResponse.cs
@@ -1,26 +1,25 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
-namespace Microsoft.Azure.SignalR.Common
+namespace Microsoft.Azure.SignalR.Common;
+
+/// <summary>
+/// The response object containing the dynamic access key for signing client tokens.
+/// </summary>
+public class AccessKeyResponse
 {
     /// <summary>
-    /// The response object containing the dynamic access key for signing client tokens.
+    /// The string value of the access key for SignalR app server to sign client tokens.
     /// </summary>
-    public class AccessKeyResponse
-    {
-        /// <summary>
-        /// The string value of the access key for SignalR app server to sign client tokens.
-        /// </summary>
-        [JsonProperty("AccessKey")]
-        public string AccessKey { get; set; }
+    [JsonPropertyName("accessKey")]
+    public string AccessKey { get; set; }
 
-        /// <summary>
-        /// The ID of the access key.
-        /// </summary>
-        [JsonProperty("KeyId")]
-        public string KeyId { get; set; }
-    }
+    /// <summary>
+    /// The ID of the access key.
+    /// </summary>
+    [JsonPropertyName("keyId")]
+    public string KeyId { get; set; }
 }
 
