155f8a9dccafa4c82ecd10f056904052dc027b45
stdrickforce <stdrickforce@gmail.com>
2024-10-29 09:06:29 +0800

    apply file-scoped namespace to Auth classes

--- src/Microsoft.Azure.SignalR.Common/Auth/JwtHeader.cs
+++ src/Microsoft.Azure.SignalR.Common/Auth/JwtHeader.cs
@@ -12,53 +12,52 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Text;
 
-namespace Microsoft.Azure.SignalR
+namespace Microsoft.Azure.SignalR;
+
+internal class JwtHeader : Dictionary<string, object>
 {
-    internal class JwtHeader : Dictionary<string, object>
+    /// <summary>
+    /// Create a <see cref="JwtHeader"/> representing JWT header {"alg":<paramref name="algorithm"/>,"typ":"JWT","kid":<paramref name="kid"/>}
+    /// </summary>
+    /// <returns> JWT header</returns>
+    public JwtHeader(string kid, AccessTokenAlgorithm algorithm)
     {
-        /// <summary>
-        /// Create a <see cref="JwtHeader"/> representing JWT header {"alg":<paramref name="algorithm"/>,"typ":"JWT","kid":<paramref name="kid"/>}
-        /// </summary>
-        /// <returns> JWT header</returns>
-        public JwtHeader(string kid, AccessTokenAlgorithm algorithm)
+        // Write parameter `alg`
+        switch (algorithm)
         {
-            // Write parameter `alg`
-            switch (algorithm)
-            {
-                case AccessTokenAlgorithm.HS256:
-                    this["alg"] = "HS256";
-                    break;
-                case AccessTokenAlgorithm.HS512:
-                    this["alg"] = "HS512";
-                    break;
-                default:
-                    throw new NotSupportedException("Not Supported Encryption Algorithm for JWT Token");
-            }
-
-            // Write parameter `typ` and `kid`
-            this["typ"] = "JWT";
-            if (kid != null)
-            {
-                this["kid"] = kid;
-            }
+            case AccessTokenAlgorithm.HS256:
+                this["alg"] = "HS256";
+                break;
+            case AccessTokenAlgorithm.HS512:
+                this["alg"] = "HS512";
+                break;
+            default:
+                throw new NotSupportedException("Not Supported Encryption Algorithm for JWT Token");
         }
 
-        /// Simplified from https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/6.22.0/src/System.IdentityModel.Tokens.Jwt/JwtHeader.cs#L328 and https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/6.22.0/src/System.IdentityModel.Tokens.Jwt/JsonExtensions.cs
-        /// <summary>
-        /// Convert this <see cref="JwtHeader"/> to corresponding Base64Url encoding
-        /// </summary>
-        /// <returns>Base64Url encoding of a <see cref="JwtHeader"/></returns>
-        public string Base64UrlEncode()
+        // Write parameter `typ` and `kid`
+        this["typ"] = "JWT";
+        if (kid != null)
         {
-            string json = JsonConvert.SerializeObject(this as IDictionary<string, object>);
-            if (json == null)
-            {
-                throw LogHelper.LogArgumentNullException("json");
-            }
-
-            byte[] bytes = Encoding.UTF8.GetBytes(json);
-
-            return Base64UrlEncoder.Encode(bytes);
+            this["kid"] = kid;
         }
+    }
+
+    /// Simplified from https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/6.22.0/src/System.IdentityModel.Tokens.Jwt/JwtHeader.cs#L328 and https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/6.22.0/src/System.IdentityModel.Tokens.Jwt/JsonExtensions.cs
+    /// <summary>
+    /// Convert this <see cref="JwtHeader"/> to corresponding Base64Url encoding
+    /// </summary>
+    /// <returns>Base64Url encoding of a <see cref="JwtHeader"/></returns>
+    public string Base64UrlEncode()
+    {
+        string json = JsonConvert.SerializeObject(this as IDictionary<string, object>);
+        if (json == null)
+        {
+            throw LogHelper.LogArgumentNullException("json");
+        }
+
+        byte[] bytes = Encoding.UTF8.GetBytes(json);
+
+        return Base64UrlEncoder.Encode(bytes);
     }
 }
