00e73817f28e0d345382ea5df222d43683c60f41
Terence Fan <stdrickforce@gmail.com>
2024-11-01 13:01:11 +0800

    removed

--- src/Microsoft.Azure.SignalR.Common/Auth/AccessKey.cs
+++ src/Microsoft.Azure.SignalR.Common/Auth/AccessKey.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
