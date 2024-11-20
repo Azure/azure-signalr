82d9d2610b6fc0854aa92cc3b52eec611ef52958
Liangying Wei <lianwei@microsoft.com>
2024-11-20 15:39:06 +0800

    Adding pitfalls

---
+++ src/Microsoft.Azure.SignalR.Protocols/MessagePackPitfalls.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+internal sealed class DoesNotReturnAttribute : Attribute
+{
+}
