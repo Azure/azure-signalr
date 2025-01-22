06f158a8ca5de64b81aa845a5cbfc0ff9109e38a
stdrickforce <stdrickforce@gmail.com>
2025-01-22 11:06:10 +0800

    add RetryFact

--- test/Microsoft.Azure.SignalR.Tests/AckHandlerTest.cs
+++ test/Microsoft.Azure.SignalR.Tests/AckHandlerTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Threading.Tasks;
 
 using Xunit;
@@ -8,16 +11,16 @@ namespace Microsoft.Azure.SignalR.Tests;
 public class AckHandlerTest
 {
     [Fact]
-    public void TestOnce()
+    public async Task TestOnce()
     {
         var handler = new AckHandler();
         var task = handler.CreateSingleAck(out var ackId);
         handler.TriggerAck(ackId);
         Assert.True(task.IsCompletedSuccessfully);
-        Assert.Equal(AckStatus.Ok, task.Result);
+        Assert.Equal(AckStatus.Ok, await task);
     }
 
-    [Fact]
+    [RetryFact]
     public async Task TestOnce_Timeout()
     {
         var handler = new AckHandler(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(20));
@@ -65,7 +68,7 @@ public class AckHandlerTest
         await Task.Delay(TimeSpan.FromSeconds(1.5));
         Assert.True(task.IsCompleted);
         // This assertion is different from RT for different behaviour when timeout of AckHandler. See annotation in AckHandler.cs method CheckAcs
-        Assert.Equal(AckStatus.Timeout, task.Result);
+        Assert.Equal(AckStatus.Timeout, await task);
     }
 
     [Fact]
