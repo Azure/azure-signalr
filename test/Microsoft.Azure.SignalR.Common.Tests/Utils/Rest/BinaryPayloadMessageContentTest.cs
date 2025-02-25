c506216cb8473a88bc574776346463de81b83752
Terence Fan <stdrickforce@gmail.com>
2025-02-25 09:47:10 +0800

    update

--- test/Microsoft.Azure.SignalR.Common.Tests/Utils/Rest/BinaryPayloadMessageContentTest.cs
+++ test/Microsoft.Azure.SignalR.Common.Tests/Utils/Rest/BinaryPayloadMessageContentTest.cs
@@ -6,7 +6,6 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 using MessagePack;
 
@@ -19,13 +18,14 @@ namespace Microsoft.Azure.SignalR.Common.Tests;
 public class BinaryPayloadMessageContentTest
 {
     [Fact]
-    public async Task OneHubProtocolTest()
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1031:Do not use blocking task operations in test method", Justification = "<Pending>")]
+    public void OneHubProtocolTest()
     {
-        var payload = new PayloadMessage { Target = "target", Arguments = ["a", 1] };
+        var payload = new PayloadMessage { Target = "target", Arguments = new object[] { "a", 1 } };
         var protocols = new List<IHubProtocol>() { new MessagePackHubProtocol() };
         using var httpContent = new BinaryPayloadMessageContent(payload, protocols);
         var actualBytes = new MemoryStream();
-        await httpContent.CopyToAsync(actualBytes, null, default);
+        httpContent.CopyToAsync(actualBytes, null, default).GetAwaiter().GetResult();
         var expectedBytes = new ArrayBufferWriter<byte>();
         var messagePackWriter = new MessagePackWriter(expectedBytes);
         messagePackWriter.WriteMapHeader(1);
@@ -36,13 +36,14 @@ public class BinaryPayloadMessageContentTest
     }
 
     [Fact]
-    public async Task TwoHubProtocolTest()
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1031:Do not use blocking task operations in test method", Justification = "<Pending>")]
+    public void TwoHubProtocolTest()
     {
-        var payload = new PayloadMessage { Target = "target", Arguments = ["a", 1] };
+        var payload = new PayloadMessage { Target = "target", Arguments = new object[] { "a", 1 } };
         var protocols = new List<IHubProtocol>() { new MessagePackHubProtocol(), new JsonHubProtocol() };
         using var httpContent = new BinaryPayloadMessageContent(payload, protocols);
         var actualBytes = new MemoryStream();
-        await httpContent.CopyToAsync(actualBytes, null, default);
+        httpContent.CopyToAsync(actualBytes, null, default).GetAwaiter().GetResult();
         var expectedBytes = new ArrayBufferWriter<byte>();
         var messagePackWriter = new MessagePackWriter(expectedBytes);
         messagePackWriter.WriteMapHeader(2);
