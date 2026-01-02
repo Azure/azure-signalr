b2405099ffc727c5483c4f2418556be17defdbb7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-02 15:47:24 -0500

    spelling: negotiate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/Microsoft.Azure.SignalR.Common/Endpoints/ServerStickyMode.cs
+++ src/Microsoft.Azure.SignalR.Common/Endpoints/ServerStickyMode.cs
@@ -16,7 +16,7 @@ public enum ServerStickyMode
     Disabled = 0,
 
     ///// <summary>
-    ///// We will try to find the server it /neogitate with from local, if that server is connected to this runtime instance, we choose that server
+    ///// We will try to find the server it /negotiate with from local, if that server is connected to this runtime instance, we choose that server
     ///// Otherwise, we fall back to local existed server
     ///// </summary>
     Preferred = 1,
